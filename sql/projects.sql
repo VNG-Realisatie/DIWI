3b2832b633dd674df1736230cc7bbe46647bf54d
Emiel Rombouts <erombouts@phinion.com>
2023-08-29 14:08:06 +0200

    Add project fase

--- sql/projects.sql
+++ sql/projects.sql
@@ -19,19 +19,34 @@ WITH woningblokken AS (
         LEFT JOIN diwi_testset_simplified.woningblok_naam_changelog wnc ON wnc."woningblok_ID" = w."ID"
             AND wnc.change_end_date IS NULL
 ),
+current_milestone AS (
+    SELECT
+        pfc.*        
+    FROM
+        diwi_testset_simplified.project_fase_changelog pfc
+    LEFT JOIN diwi_testset_simplified.milestone sm ON pfc."start_milestone_ID" = sm."ID"
+        LEFT JOIN diwi_testset_simplified.milestone_state sms ON sms."milestone_ID" = sm."ID"
+        LEFT JOIN diwi_testset_simplified.milestone em ON pfc."end_milestone_iD" = em."ID"
+        LEFT JOIN diwi_testset_simplified.milestone_state ems ON ems."milestone_ID" = em."ID"
+    WHERE
+        sms.status = 'gerealiseerd'
+        AND ems.status != 'gerealiseerd'
+),
 projecten AS (
     SELECT
         p."ID" AS id,
         pnc."name" AS "name",
         os.naam AS "eigenaar",
         ps.confidentiality_level AS "vertrouwlijkheidsniveau",
-        pgvs.value_label AS "rol gemeente",
         ppvs.value_label AS "priorisering",
         pptc.plan_type AS "plan type",
         milestone_start_state."date" AS "start datum",
         milestone_end_state."date" AS "eind datum",
+        pgvs.value_label AS "rol gemeente",
+        cm.project_fase AS "project fase",
         -- extra id's for debugging, might not be needed for UI
         ps."ID" AS project_state_id,
+        cm."ID" AS project_fase_changelog_id,
         pnc."ID" AS project_name_changelog_id,
         o."ID" AS organization_id,
         os."ID" AS organization_state_id,
@@ -43,33 +58,36 @@ projecten AS (
         ppvs."ID" AS project_priorisering_value_state_id
     FROM
         diwi_testset_simplified.project AS p
-    LEFT JOIN diwi_testset_simplified.project_name_changelog AS pnc ON pnc."project_ID" = p."ID"
-        AND pnc.change_end_date IS NULL
-    LEFT JOIN diwi_testset_simplified.project_state AS ps ON ps."project_ID" = p."ID"
-        AND ps.change_end_date IS NULL
-    LEFT JOIN diwi_testset_simplified.organization AS o ON o."ID" = ps."owner_organization_ID"
+        LEFT JOIN diwi_testset_simplified.project_name_changelog AS pnc ON pnc."project_ID" = p."ID"
+        LEFT JOIN diwi_testset_simplified.project_state AS ps ON ps."project_ID" = p."ID"
+        LEFT JOIN diwi_testset_simplified.organization AS o ON o."ID" = ps."owner_organization_ID"
         LEFT JOIN diwi_testset_simplified.organization_state AS os ON os."organization_ID" = o."ID"
-            AND os.change_end_date IS NULL
         LEFT JOIN diwi_testset_simplified.project_gemeenterol_changelog AS pgc ON pgc."project_ID" = p."ID"
-            AND pgc.change_end_date IS NULL
         LEFT JOIN diwi_testset_simplified.project_gemeenterol_value AS pgv ON pgv."ID" = pgc."project_gemeenterol_value_ID"
         LEFT JOIN diwi_testset_simplified.project_gemeenterol_value_state AS pgvs ON pgvs."project_gemeenterol_value_ID" = pgv."ID"
-            AND pgvs.change_end_date IS NULL
         LEFT JOIN diwi_testset_simplified.project_priorisering_changelog ppc ON ppc."project_ID" = p."ID"
-            AND ppc.change_end_date IS NULL
         LEFT JOIN diwi_testset_simplified.project_priorisering_value ppv ON ppv."ID" = ppc."project_priorisering_value_ID"
         LEFT JOIN diwi_testset_simplified.project_priorisering_value_state ppvs ON ppvs."project_priorisering_value_ID" = ppv."ID"
-            AND ppvs.change_end_date IS NULL
         LEFT JOIN diwi_testset_simplified.project_duration_changelog pdc ON pdc."project_ID" = p."ID"
-            AND pdc.change_end_date IS NULL
         LEFT JOIN diwi_testset_simplified.project_plan_type_changelog pptc ON pptc."project_ID" = p."ID"
-            AND pptc.change_end_date IS NULL
         LEFT JOIN diwi_testset_simplified.milestone milestone_start ON milestone_start."ID" = pptc."start_milestone_ID"
         LEFT JOIN diwi_testset_simplified.milestone_state milestone_start_state ON milestone_start_state."milestone_ID" = milestone_start."ID"
-            AND milestone_start_state.change_end_date IS NULL
         LEFT JOIN diwi_testset_simplified.milestone milestone_end ON milestone_start."ID" = pptc."end_milestone_iD"
         LEFT JOIN diwi_testset_simplified.milestone_state milestone_end_state ON milestone_end_state."milestone_ID" = milestone_start."ID"
-            AND milestone_end_state.change_end_date IS NULL
+        LEFT JOIN current_milestone cm ON cm."project_ID" = p."ID"
+    WHERE
+        pnc.change_end_date IS NULL
+        AND ps.change_end_date IS NULL
+        AND os.change_end_date IS NULL
+        AND pgc.change_end_date IS NULL
+        AND pgvs.change_end_date IS NULL
+        AND ppc.change_end_date IS NULL
+        AND ppvs.change_end_date IS NULL
+        AND pdc.change_end_date IS NULL
+        AND pptc.change_end_date IS NULL
+        AND milestone_start_state.change_end_date IS NULL
+        AND milestone_end_state.change_end_date IS NULL
+        AND cm.change_end_date IS NULL
 ),
 plannen AS (
     SELECT
@@ -79,12 +97,12 @@ plannen AS (
         plan_state.doel_waarde AS doel_waarde
     FROM
         diwi_testset_simplified.plan AS plan
-    INNER JOIN diwi_testset_simplified.plan_state AS plan_state ON plan_state."plan_ID" = plan."ID"
+        INNER JOIN diwi_testset_simplified.plan_state AS plan_state ON plan_state."plan_ID" = plan."ID"
 ),
 projecten_with_woningblokken AS (
     SELECT
         to_jsonb (p) AS project,
-    COALESCE(json_agg(w.*) FILTER (WHERE w.id IS NOT NULL), '[]') AS woningblokken
+        COALESCE(json_agg(w.*) FILTER (WHERE w.id IS NOT NULL), '[]') AS woningblokken
 FROM
     projecten AS p
         LEFT JOIN woningblokken AS w ON w.project_id = p.id
