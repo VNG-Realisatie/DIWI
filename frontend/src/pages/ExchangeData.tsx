4373c14abf71f6515f200e3116d0b9acec2ed672
Ali Demirci <ademirci@phinion.com>
2023-09-04 12:03:58 +0200

    Add squit import to the exchange data page

--- frontend/src/pages/ExchangeData.tsx
+++ frontend/src/pages/ExchangeData.tsx
@@ -20,7 +20,7 @@ export const ExchangeData = () => {
         <DataCardItem text="Excel" link={Paths.importExcel.path}  isImport >
             <img src={excelIcon} alt='excel'/>
         </DataCardItem>
-        <DataCardItem text="Squit" link="" isImport >
+        <DataCardItem text="Squit" link={Paths.importSquit.path} isImport >
             <img src={roxitIcon} alt='squit'/>
         </DataCardItem>
         <DataCardItem text="Systeem x" link=""  isImport >
