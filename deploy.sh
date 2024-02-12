ce58af77752e17d8ab6c29eb77e0303eb273e068
Emiel Rombouts <erombouts@phinion.com>
2024-02-12 08:40:43 +0100

    Auto pull on deploy like all other projects

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-touch htpasswd # Create the file if it isn't there yet, otherwise docker will create a directory
+git pull
 
 docker compose pull
 [ $? -eq 0 ] && docker compose up --build --remove-orphans -d
