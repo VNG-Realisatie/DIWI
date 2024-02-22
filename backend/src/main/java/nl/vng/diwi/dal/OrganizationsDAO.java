3a694884133dc090f56d94b0573bab7acb750018
Daniela Bazacliu <dbazacliu@phinion.com>
2024-02-23 00:05:14 +0200

    Update project owners and leaders, add project service test

--- backend/src/main/java/nl/vng/diwi/dal/OrganizationsDAO.java
+++ backend/src/main/java/nl/vng/diwi/dal/OrganizationsDAO.java
@@ -15,31 +15,31 @@ public class OrganizationsDAO extends AbstractRepository {
 
     public List<OrganizationUserModel> getOrganizationUsersList() {
 
-        return session.createNativeQuery("""
+        return session.createNativeQuery(String.format("""
                 SELECT o.id AS organizationUuid,
                     os.naam AS organizationName,
                     us.id AS uuid,
                     us.last_name AS lastName,
                     us.first_name AS firstName,
                     LEFT(us.last_name, 1) || LEFT(us.first_name,1) AS initials
-                FROM diwi_testset.organization o
-                    JOIN diwi_testset.organization_state os ON o.id = os.organization_id AND os.change_end_date IS NULL
-                    JOIN diwi_testset.user_to_organization uo ON uo.organization_id = os.organization_id AND uo.change_end_date IS NULL
-                    JOIN diwi_testset.user_state us ON us.user_id = uo.user_id AND us.change_end_date IS NULL
-                ORDER BY organizationName, initials, lastName, firstName """, Object[].class)
+                FROM %1$s.organization o
+                    JOIN %1$s.organization_state os ON o.id = os.organization_id AND os.change_end_date IS NULL
+                    JOIN %1$s.user_to_organization uo ON uo.organization_id = os.organization_id AND uo.change_end_date IS NULL
+                    JOIN %1$s.user_state us ON us.user_id = uo.user_id AND us.change_end_date IS NULL
+                ORDER BY organizationName, initials, lastName, firstName """, GenericRepository.VNG_SCHEMA_NAME), Object[].class)
             .setTupleTransformer(new BeanTransformer<>(OrganizationUserModel.class))
             .list();
 
     }
 
     public UUID findOrganizationForProject(UUID projectUuid, UUID organizationUuid, ProjectRole projectRole) {
-        return session.createNativeQuery("""
+        return session.createNativeQuery(String.format("""
                 SELECT id
-                    FROM diwi_testset.organization_to_project
+                    FROM %1$s.organization_to_project
                 WHERE project_id = :projectId
                     AND organization_id = :organizationId
-                    AND project_rol = CAST(:projectRole AS diwi_testset.project_rol)
-                    AND change_end_date IS NULL""", UUID.class)
+                    AND project_rol = CAST(:projectRole AS %1$s.project_rol)
+                    AND change_end_date IS NULL""", GenericRepository.VNG_SCHEMA_NAME), UUID.class)
             .setParameter("projectId", projectUuid)
             .setParameter("organizationId", organizationUuid)
             .setParameter("projectRole", projectRole.name())
@@ -47,9 +47,9 @@ public class OrganizationsDAO extends AbstractRepository {
     }
 
     public void addOrganizationToProject(UUID projectUuid, UUID organizationToAdd, ProjectRole projectRole, UUID loggedInUserUuid) {
-        session.createNativeMutationQuery("""
-                INSERT INTO diwi_testset.organization_to_project (id, organization_id, project_id, project_rol, change_start_date, change_user_id)
-                    VALUES (:id, :organizationId, :projectId, CAST(:projectRole AS diwi_testset.project_rol), NOW(), :changeUserId) """)
+        session.createNativeMutationQuery(String.format("""
+                INSERT INTO %1$s.organization_to_project (id, organization_id, project_id, project_rol, change_start_date, change_user_id)
+                    VALUES (:id, :organizationId, :projectId, CAST(:projectRole AS %1$s.project_rol), NOW(), :changeUserId) """, GenericRepository.VNG_SCHEMA_NAME))
             .setParameter("id", CustomUuidGenerator.generateUUIDv7())
             .setParameter("organizationId", organizationToAdd)
             .setParameter("projectId", projectUuid)
@@ -59,13 +59,13 @@ public class OrganizationsDAO extends AbstractRepository {
     }
 
     public void removeOrganizationFromProject(UUID projectUuid, UUID organizationToRemove, ProjectRole projectRole) {
-        session.createNativeMutationQuery("""
-                UPDATE diwi_testset.organization_to_project
+        session.createNativeMutationQuery(String.format("""
+                UPDATE %1$s.organization_to_project
                     SET change_end_date = NOW()
                 WHERE organization_id = :organizationId
                     AND project_id = :projectId
-                    AND project_rol = CAST(:projectRole AS diwi_testset.project_rol)
-                    AND change_end_date IS NULL """)
+                    AND project_rol = CAST(:projectRole AS %1$s.project_rol)
+                    AND change_end_date IS NULL """, GenericRepository.VNG_SCHEMA_NAME))
             .setParameter("organizationId", organizationToRemove)
             .setParameter("projectId", projectUuid)
             .setParameter("projectRole", projectRole.name())
