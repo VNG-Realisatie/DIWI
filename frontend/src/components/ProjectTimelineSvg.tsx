8e1b751c3ab88cc08d0a4e2ad7106ffd07812cfb
Daniil Kravchenko <dkravchenko@phinion.com>
2024-02-26 15:24:31 +0100

    adds title rectangle and month divider

--- frontend/src/components/ProjectTimelineSvg.tsx
+++ frontend/src/components/ProjectTimelineSvg.tsx
@@ -11,8 +11,6 @@ const ProjectTimelineSvg = ({ projectData, dateRange, timeFormat, width }: any) 
         const margin = { top: 0, right: 20, bottom: 120, left: 20 };
         const rectHeight = 80;
 
-        if (!projectData) return;
-
         const svg = d3
             .select(svgRef.current)
             .attr("width", width + margin.left + margin.right)
@@ -37,6 +35,11 @@ const ProjectTimelineSvg = ({ projectData, dateRange, timeFormat, width }: any) 
         // Append a rectangle for the background
         monthTextGroup.append("rect").attr("width", width).attr("height", 20).attr("fill", "lightgrey").attr("opacity", 0.3);
 
+        const projectFasenTitle = svg.append("g");
+        projectFasenTitle.append("rect").attr("width", width).attr("height", 20).attr("y", 20).attr("fill", "rgb(231,184,93)");
+
+        projectFasenTitle.append("text").attr("x", 10).attr("y", 35).attr("fill", "white").text("Projectfasen");
+
         monthTextGroup
             .selectAll(".month-text")
             .data(dateRange)
@@ -47,6 +50,18 @@ const ProjectTimelineSvg = ({ projectData, dateRange, timeFormat, width }: any) 
             .attr("y", 15)
             .text((d: any) => d3.timeFormat(timeFormat)(d));
 
+        monthTextGroup
+            .selectAll(".month-divider")
+            .data(dateRange.slice(0, -1))
+            .enter()
+            .append("line")
+            .attr("class", "month-divider")
+            .attr("x1", (d: any) => xScale(d))
+            .attr("x2", (d: any) => xScale(d))
+            .attr("y1", 0)
+            .attr("y2", 20)
+            .attr("stroke", "white")
+            .attr("stroke-width", 3);
         const projectNames = projectData.projectName || []; //huisblokken ???
         const projectPhases = projectData.projectPhase || [];
 
@@ -57,7 +72,7 @@ const ProjectTimelineSvg = ({ projectData, dateRange, timeFormat, width }: any) 
                 .append("rect")
                 .attr("class", className)
                 .attr("x", (d: any, i: number) => xScale(new Date(d.startDate)) + i * 1.01)
-                .attr("y", 30)
+                .attr("y", 40)
                 .attr("width", (d: any) => xScale(new Date(d.endDate)) - xScale(new Date(d.startDate)))
                 .attr("height", rectHeight)
                 .attr("fill", color)
