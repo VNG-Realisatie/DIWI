32543d1d25b69106aa5ed3e03d4195bbdffd2b9c
Ali D <ali@phinion.com>
2024-03-07 13:52:15 +0100

    Change select to autocomplete in municipality role edit page

--- frontend/src/components/project/project-with-house-block/MunicipalityRoleEditForm.tsx
+++ frontend/src/components/project/project-with-house-block/MunicipalityRoleEditForm.tsx
@@ -1,46 +1,30 @@
-import { Checkbox, ListItemText, MenuItem, OutlinedInput, Select, SelectChangeEvent } from "@mui/material";
+import { Autocomplete, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getMunicipalityRoleList } from "../../../api/projectsTableServices";
-import { MenuProps } from "../../../utils/menuProps";
-import { OptionType } from "../ProjectsTableView";
+import { SelectModel } from "../../../api/projectsServices";
 
 type Props = {
-    selectedMunicipalityRole: string[];
-    setSelectedMunicipalityRole: (mr: string[]) => void;
+    selectedMunicipalityRole: SelectModel[];
+    setSelectedMunicipalityRole: (mr: SelectModel[]) => void;
 };
 
 export const MunicipalityRoleEditForm = ({ selectedMunicipalityRole, setSelectedMunicipalityRole }: Props) => {
-    const [municipalityRolesOptions, setMunicipalityRolesOptions] = useState<OptionType[]>();
-
-    const handleMunicipalityRoleChange = (event: SelectChangeEvent<typeof selectedMunicipalityRole>) => {
-        const {
-            target: { value },
-        } = event;
-        setSelectedMunicipalityRole(typeof value === "string" ? [value] : value);
-    };
+    const [municipalityRolesOptions, setMunicipalityRolesOptions] = useState<SelectModel[]>();
 
     useEffect(() => {
         getMunicipalityRoleList().then((roles) => setMunicipalityRolesOptions(roles));
     }, []);
 
     return (
-        <Select
-            fullWidth
-            size="small"
-            id="municipality-role-checkbox"
+        <Autocomplete
             multiple
+            id="tags-outlined"
+            options={municipalityRolesOptions ? municipalityRolesOptions : []}
+            getOptionLabel={(option) => option.name}
             value={selectedMunicipalityRole}
-            onChange={handleMunicipalityRoleChange}
-            input={<OutlinedInput />}
-            renderValue={(selected) => selected.join(", ")}
-            MenuProps={MenuProps}
-        >
-            {municipalityRolesOptions?.map((municipality) => (
-                <MenuItem key={municipality.id} value={municipality.name}>
-                    <Checkbox checked={selectedMunicipalityRole.indexOf(municipality.name) !== -1} />
-                    <ListItemText primary={municipality.name} />
-                </MenuItem>
-            ))}
-        </Select>
+            filterSelectedOptions
+            onChange={(_: any, newValue: SelectModel[]) => setSelectedMunicipalityRole(newValue)}
+            renderInput={(params) => <TextField {...params} />}
+        />
     );
 };
