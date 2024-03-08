fb03db353cb127d2e7de3719e42a12eb1274366d
Ali D <ali@phinion.com>
2024-03-08 14:55:21 +0100

    Create demolition capacity input

---
+++ frontend/src/components/project-wizard/house-blocks/mutation-information/DemolitionPlanCapacityInput.tsx
@@ -0,0 +1,32 @@
+import { Stack, Typography, TextField } from "@mui/material";
+import { t } from "i18next";
+import { MutationInformationProps } from "./MutationInformationGroup";
+
+export const DemolitionPlanCapacityInput = ({ projectForm, setProjectForm }: MutationInformationProps) => {
+    return (
+        <Stack>
+            <Typography variant="subtitle1" fontWeight="500">
+                {t("createProject.houseBlocksForm.demolition")}
+            </Typography>
+            <TextField
+                InputProps={{
+                    inputProps: {
+                        min: 0,
+                    },
+                }}
+                type="number"
+                id="demolitionPlan"
+                size="small"
+                variant="outlined"
+                value={projectForm ? projectForm.demolition : null}
+                onChange={(e) =>
+                    projectForm &&
+                    setProjectForm({
+                        ...projectForm,
+                        demolition: +e.target.value,
+                    })
+                }
+            />
+        </Stack>
+    );
+};
