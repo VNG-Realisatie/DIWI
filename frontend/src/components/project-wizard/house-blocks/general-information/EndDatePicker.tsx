4ecc98d1e28f86f04846d2ade70f55ba60f7d8f2
Ali D <ali@phinion.com>
2024-03-14 09:31:08 +0100

    Disable end date if edit not clicked

--- frontend/src/components/project-wizard/house-blocks/general-information/EndDatePicker.tsx
+++ frontend/src/components/project-wizard/house-blocks/general-information/EndDatePicker.tsx
@@ -3,22 +3,41 @@ import { DatePicker } from "@mui/x-date-pickers";
 import { t } from "i18next";
 import { GeneralInformationProps } from "./GeneralInformationGroup";
 import dayjs from "dayjs";
+import { InputContainer } from "../InputContainer";
 
-export const EndDatePicker = ({ projectForm, setProjectForm }: GeneralInformationProps) => {
+export const EndDatePicker = ({ projectForm, setProjectForm, edit, editForm }: GeneralInformationProps) => {
     return (
         <Stack width="100%">
             <Typography variant="subtitle1" fontWeight="500">
                 {t("createProject.houseBlocksForm.endDate")}
             </Typography>
-            <DatePicker
-                value={projectForm ? dayjs(projectForm.endDate) : null}
-                onChange={(e) =>
-                    setProjectForm({
-                        ...projectForm,
-                        endDate: e instanceof Date ? e.toISOString() : "",
-                    })
-                }
-            />
+            {edit && editForm && (
+                <DatePicker
+                    value={projectForm ? dayjs(projectForm.endDate) : null}
+                    onChange={(e) =>
+                        setProjectForm({
+                            ...projectForm,
+                            endDate: e instanceof Date ? e.toISOString() : "",
+                        })
+                    }
+                />
+            )}
+            {!edit && editForm && (
+                <InputContainer>
+                    <Typography>{projectForm?.endDate}</Typography>
+                </InputContainer>
+            )}
+            {!edit && !editForm && (
+                <DatePicker
+                    value={projectForm ? dayjs(projectForm.endDate) : null}
+                    onChange={(e) =>
+                        setProjectForm({
+                            ...projectForm,
+                            endDate: e instanceof Date ? e.toISOString() : "",
+                        })
+                    }
+                />
+            )}
         </Stack>
     );
 };
