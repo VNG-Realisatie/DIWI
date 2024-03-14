9a41696da098af07ad72d18f6b07e56fa98e5bbf
Ali D <ali@phinion.com>
2024-03-14 09:09:18 +0100

    Disable size input if edit not clicked

--- frontend/src/components/project-wizard/house-blocks/general-information/SizeInput.tsx
+++ frontend/src/components/project-wizard/house-blocks/general-information/SizeInput.tsx
@@ -1,8 +1,9 @@
 import { Stack, TextField, Typography } from "@mui/material";
 import { t } from "i18next";
 import { GeneralInformationProps } from "./GeneralInformationGroup";
+import { InputContainer } from "../InputContainer";
 
-export const SizeInput = ({ projectForm, setProjectForm }: GeneralInformationProps) => {
+export const SizeInput = ({ projectForm, setProjectForm, edit, editForm }: GeneralInformationProps) => {
     const handleSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newSize = {
             value: +e.target.value,
@@ -20,19 +21,41 @@ export const SizeInput = ({ projectForm, setProjectForm }: GeneralInformationPro
             <Typography variant="subtitle1" fontWeight="500">
                 {t("createProject.houseBlocksForm.size")}
             </Typography>
-            <TextField
-                InputProps={{
-                    inputProps: {
-                        min: 0,
-                    },
-                }}
-                type="number"
-                id="size"
-                size="small"
-                variant="outlined"
-                value={projectForm ? projectForm?.size?.value : 0}
-                onChange={handleSizeChange}
-            />
+            {edit && editForm && (
+                <TextField
+                    InputProps={{
+                        inputProps: {
+                            min: 0,
+                        },
+                    }}
+                    type="number"
+                    id="size"
+                    size="small"
+                    variant="outlined"
+                    value={projectForm ? projectForm?.size?.value : 0}
+                    onChange={handleSizeChange}
+                />
+            )}
+            {!edit && editForm && (
+                <InputContainer>
+                    <Typography>{projectForm?.size?.value}</Typography>
+                </InputContainer>
+            )}
+            {!edit && !editForm && (
+                <TextField
+                    InputProps={{
+                        inputProps: {
+                            min: 0,
+                        },
+                    }}
+                    type="number"
+                    id="size"
+                    size="small"
+                    variant="outlined"
+                    value={projectForm ? projectForm?.size?.value : 0}
+                    onChange={handleSizeChange}
+                />
+            )}
         </Stack>
     );
 };
