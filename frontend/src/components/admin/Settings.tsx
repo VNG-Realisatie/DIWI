497565581133fa90ca280cd436e6e2021c09fd46
Ali D <ali@phinion.com>
2024-03-17 02:04:35 +0100

    Localization updates and refactoring

--- frontend/src/components/admin/Settings.tsx
+++ frontend/src/components/admin/Settings.tsx
@@ -1,29 +1,12 @@
-import {
-    Button,
-    Checkbox,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    MenuItem,
-    Select,
-    SelectChangeEvent,
-    Stack,
-    Switch,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    TextField,
-    Typography,
-} from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { useTranslation } from "react-i18next";
-import { ChangeEvent, useEffect, useState } from "react";
-import { CustomPropertyType, getCustomProperties } from "../../api/adminSettingServices";
-import { objectType, propertyType } from "./constants";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { CustomPropertyType, addCustomProperty, getCustomProperties } from "../../api/adminSettingServices";
+import { ObjectType, PropertyType } from "../../types/enums";
+import AlertContext from "../../context/AlertContext";
+import { CreatePropertyDialog } from "./CreatePropertyDialog";
+import { CustomPropertiesTable } from "./CustomPropertiesTable";
 
 export const rowStyle = {
     p: 1,
@@ -31,128 +14,66 @@ export const rowStyle = {
 };
 export const Settings = () => {
     const [openDialog, setOpenDialog] = useState(false);
-    const [customProperties, setCustomProperties] = useState<CustomPropertyType[]>();
-    const [selectedObjectType, setSelectedObjectType] = useState<string>();
-    const [selectedPropertyType, setSelectedPropertyType] = useState<string>();
+    const [customProperties, setCustomProperties] = useState<CustomPropertyType[]>([]);
+    const [selectedObjectType, setSelectedObjectType] = useState<ObjectType>("PROJECT");
+    const [selectedPropertyType, setSelectedPropertyType] = useState<PropertyType>("TEXT");
     const [active, setActive] = useState(false);
-    const [name, setName] = useState<string>();
-    const headerStyle = {
-        color: "#ffffff",
-        fontWeight: "600",
-        border: "solid 1px #BDBDBD",
-    };
-    const cellStyle = {
-        border: "solid 1px #BDBDBD",
-    };
+    const [name, setName] = useState<string>("");
+    const [categories, setCategories] = useState<string[]>([]);
 
     const { t } = useTranslation();
-    const handleSave = () => {
-        console.log("");
-    };
+    const { setAlert } = useContext(AlertContext);
+
+    const handleSave = useCallback(() => {
+        const newProperty = {
+            name,
+            objectType: selectedObjectType,
+            propertyType: selectedPropertyType,
+            disabled: !active,
+            categories:
+                categories.length > 0
+                    ? categories.map((c) => {
+                          return { name: c };
+                      })
+                    : null,
+        };
+        addCustomProperty(newProperty).then(() => {
+            setAlert(t("admin.settings.notifications.successfullySaved"), "success");
+            setOpenDialog(false);
+            getCustomProperties().then((customProperties) => setCustomProperties(customProperties));
+        });
+    }, [active, categories, name, selectedObjectType, selectedPropertyType, setAlert, t]);
+
     useEffect(() => {
         getCustomProperties().then((customProperties) => setCustomProperties(customProperties));
     }, []);
+
+    useEffect(() => {
+        selectedPropertyType !== "CATEGORY" && setCategories([]);
+    }, [selectedPropertyType]);
     return (
-        <Stack m={2}>
+        <Stack mt={2} mb={5} mx={2} pb={3}>
             <Typography fontWeight={600}>{t("admin.settings.title")}</Typography>
-            <TableContainer sx={{ mt: 2 }}>
-                <Table sx={{ minWidth: 650 }}>
-                    <TableHead sx={{ backgroundColor: "#738092" }}>
-                        <TableRow>
-                            <TableCell sx={headerStyle}>{t("admin.settings.tableHeader.name")}</TableCell>
-                            <TableCell sx={headerStyle} align="right">
-                                {t("admin.settings.tableHeader.propertyType")}
-                            </TableCell>
-                            <TableCell sx={headerStyle} align="right">
-                                {t("admin.settings.tableHeader.objectType")}
-                            </TableCell>
-                            <TableCell sx={headerStyle} align="right">
-                                {t("admin.settings.tableHeader.categories")}
-                            </TableCell>
-                            <TableCell sx={headerStyle} align="right">
-                                {t("admin.settings.tableHeader.status")}
-                            </TableCell>
-                            <TableCell sx={headerStyle} align="right">
-                                {t("admin.settings.tableHeader.edit")}
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {customProperties?.map((row: CustomPropertyType) => (
-                            <TableRow key={row.name}>
-                                <TableCell component="th" scope="row" sx={cellStyle}>
-                                    {row.name}
-                                </TableCell>
-                                <TableCell sx={cellStyle} align="right">
-                                    {row.propertyType}
-                                </TableCell>
-                                <TableCell sx={cellStyle} align="right">
-                                    {row.objectType}
-                                </TableCell>
-                                <TableCell sx={cellStyle} align="right">
-                                    {row.categories?.map((category) => <>{category}</>)}
-                                </TableCell>
-                                <TableCell sx={cellStyle} align="right">
-                                    <Switch checked={!row.disabled} color="success" />
-                                </TableCell>
-                                <TableCell sx={cellStyle} align="right">
-                                    <Button size="small" variant="contained">
-                                        {t("admin.settings.tableHeader.edit")}
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <CustomPropertiesTable customProperties={customProperties} setCustomProperties={setCustomProperties} />
             <Stack direction="row" alignItems="center" mt={1} sx={{ cursor: "pointer" }} onClick={() => setOpenDialog(true)}>
                 <AddCircleIcon color="info" sx={{ fontSize: "40px" }} />
                 {t("admin.settings.add")}
             </Stack>
-            <Dialog open={openDialog} onClose={() => setOpenDialog(false)} fullWidth>
-                <DialogTitle id="alert-dialog-title"> {t("admin.settings.add")}</DialogTitle>
-                <DialogContent>
-                    <Stack spacing={2}>
-                        <TextField
-                            label={t("admin.settings.tableHeader.name")}
-                            value={name}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-                        />
-                        <Select
-                            value={selectedObjectType}
-                            label={t("admin.settings.tableHeader.objectType")}
-                            onChange={(e: SelectChangeEvent<typeof selectedObjectType>) => setSelectedObjectType(e.target.value)}
-                        >
-                            {objectType.map((object) => {
-                                return <MenuItem value={object}>{object}</MenuItem>;
-                            })}
-                        </Select>
-                        <Select
-                            value={selectedPropertyType}
-                            label={t("admin.settings.tableHeader.propertyType")}
-                            onChange={(e: SelectChangeEvent<typeof selectedPropertyType>) => setSelectedPropertyType(e.target.value)}
-                        >
-                            {propertyType.map((property) => {
-                                return <MenuItem value={property}>{property}</MenuItem>;
-                            })}
-                        </Select>
-                        <Stack>
-                            <Typography>
-                                {t("admin.settings.tableHeader.active")}
-                                <Checkbox checked={active} onChange={() => setActive(!active)} />
-                            </Typography>
-                        </Stack>
-                    </Stack>
-                </DialogContent>
-                <DialogActions>
-                    <Button variant="contained" color="error" onClick={() => setOpenDialog(false)}>
-                        {t("generic.cancel")}
-                    </Button>
-                    <Button variant="contained" color="success" onClick={handleSave} autoFocus>
-                        {t("generic.save")}
-                    </Button>
-                </DialogActions>
-            </Dialog>
+            <CreatePropertyDialog
+                openDialog={openDialog}
+                setOpenDialog={setOpenDialog}
+                name={name}
+                setName={setName}
+                selectedObjectType={selectedObjectType}
+                setSelectedObjectType={setSelectedObjectType}
+                selectedPropertyType={selectedPropertyType}
+                setSelectedPropertyType={setSelectedPropertyType}
+                active={active}
+                setActive={setActive}
+                handleSave={handleSave}
+                categories={categories}
+                setCategories={setCategories}
+            />
         </Stack>
     );
 };
