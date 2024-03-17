1cc70009766e7d8bc2db0e31fbc9d1d1ff0f680d
Ali D <ali@phinion.com>
2024-03-17 02:03:01 +0100

    Add custom properties endpoints

--- frontend/src/api/adminSettingServices.ts
+++ frontend/src/api/adminSettingServices.ts
@@ -1,8 +1,8 @@
 import { ObjectType, PropertyType } from "../types/enums";
-import { getJson } from "../utils/requests";
+import { deleteJson, getJson, postJson, putJson } from "../utils/requests";
 import { API_URI } from "../utils/urls";
 export type CategoryType = {
-    id: string;
+    id?: string;
     name: string;
 };
 export type CustomPropertyType = {
@@ -16,4 +16,20 @@ export type CustomPropertyType = {
 
 export async function getCustomProperties(): Promise<Array<CustomPropertyType>> {
     return getJson(`${API_URI}/customproperties`);
+}
+
+export async function getCustomProperty(id: string): Promise<CustomPropertyType> {
+    return getJson(`${API_URI}/customproperties/${id}`);
+}
+
+export async function addCustomProperty(newData: CustomPropertyType): Promise<CustomPropertyType> {
+    return postJson(`${API_URI}/customproperties`, newData);
+}
+
+export async function updateCustomProperty(id: string, newData: CustomPropertyType): Promise<CustomPropertyType> {
+    return putJson(`${API_URI}/customproperties/${id}`, newData);
+}
+
+export async function deleteCustomProperty(id: string): Promise<CustomPropertyType> {
+    return deleteJson(`${API_URI}/customproperties/${id}`);
 }
