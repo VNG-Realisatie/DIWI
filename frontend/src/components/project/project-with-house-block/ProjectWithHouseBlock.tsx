f5bfa2e1e295ecc43405da0a772dc5bedec84755
Daniil Kravchenko <dkravchenko@phinion.com>
2024-03-19 15:03:50 +0100

    displays quantity of houseblocks

--- frontend/src/components/project/project-with-house-block/ProjectWithHouseBlock.tsx
+++ frontend/src/components/project/project-with-house-block/ProjectWithHouseBlock.tsx
@@ -279,9 +279,9 @@ export const ProjectsWithHouseBlock = () => {
                     <Grid item xs={6} md={1}>
                         <Typography sx={columnTitleStyle}>{t("projects.tableColumns.totalValue")}</Typography>
                         {!projectEditable ? (
-                            <CellContainer>{selectedProject?.totalValue}</CellContainer>
+                            <CellContainer>{houseBlocks ? houseBlocks.length : 0}</CellContainer>
                         ) : (
-                            <TextField size="small" disabled value={selectedProject?.totalValue} />
+                            <TextField size="small" disabled value={houseBlocks ? houseBlocks.length : 0} />
                         )}
                     </Grid>
                     <Grid item sm={2}>
