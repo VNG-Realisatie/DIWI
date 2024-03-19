3ca25d28b72d249784f92527ae7f4b4107a5ea4a
Lennart Schadron <lschadron@phinion.com>
2024-03-19 15:49:28 +0100

    Remove unused var

--- frontend/src/pages/ProjectDetail.tsx
+++ frontend/src/pages/ProjectDetail.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Tooltip, Typography } from "@mui/material";
+import { Stack, Tooltip, Typography } from "@mui/material";
 import { useContext, useState, createContext, PropsWithChildren } from "react";
 import ProjectContext from "../context/ProjectContext";
 
