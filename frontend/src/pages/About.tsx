29a036ccbf8c949d6e7bbf46bf6bac8ba8edb6bd
Lennart Schadron <lschadron@phinion.com>
2024-03-20 11:57:57 +0100

    Update About page to fetch version.json info

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,8 +1,35 @@
 import { TableContainer, Table, TableBody, TableRow, TableCell } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { diwiFetch } from "../utils/requests";
+import { useEffect, useState } from "react";
+
+type VersionType = {
+    hash: string;
+    date: string;
+};
 
 export const About = () => {
     const { t } = useTranslation();
+    const [version, setVersion] = useState<VersionType | null>(null);
+
+    useEffect(() => {
+        diwiFetch("/version.json")
+            .then((res) => {
+                if (!res.ok) {
+                    console.warn("Problem getting version info!");
+                    return null;
+                }
+                return res.json();
+            })
+            .then((data) => {
+                if (data) {
+                    const versionInfo = { hash: data["git hash"], date: data["build date"] };
+                    setVersion(versionInfo);
+                } else {
+                    setVersion(null);
+                }
+            });
+    }, []);
 
     return (
         <>
@@ -12,11 +39,11 @@ export const About = () => {
                     <TableBody>
                         <TableRow>
                             <TableCell>{t("about.version")}</TableCell>
-                            <TableCell>{process.env.REACT_APP_GIT_SHA}</TableCell>
+                            <TableCell>{version?.hash ?? "No info available"}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>{t("about.deployedOn")}</TableCell>
-                            <TableCell>{process.env.REACT_APP_DEPLOY_DATE}</TableCell>
+                            <TableCell>{version?.date ?? "No info available"}</TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
