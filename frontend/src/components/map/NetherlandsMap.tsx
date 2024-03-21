363a66f50abc8553e0dec83421454a686dbe5c51
Emiel Rombouts <erombouts@phinion.com>
2024-03-21 14:29:22 +0100

    Restoer netherlands map to version from develop

--- frontend/src/components/map/NetherlandsMap.tsx
+++ frontend/src/components/map/NetherlandsMap.tsx
@@ -5,7 +5,6 @@ import L from "leaflet";
 import "leaflet-draw";
 import { useEffect, useId, useRef } from "react";
 import { AddProjectButton } from "../PlusButton";
-import queryString from "query-string";
 
 type Marker = {
     projectColor: string;
@@ -19,17 +18,15 @@ type Props = {
     mapData: Marker[];
     plusButton?: boolean;
 };
+
 //Dummy Coordinates for the center of the Netherlands
 const center: LatLngTuple = [52.1326, 5.2913];
 
-const crs = L.CRS.EPSG4326;
 const NetherlandsMap = ({ height, width, mapData, plusButton }: Props) => {
     const mapRef = useRef<Map>();
     const id = useId();
 
     const mapZoom = 10;
-
-    fetchStuff();
 
     function createMarkerIcon(color: string) {
         return L.divIcon({
@@ -47,82 +44,30 @@ const NetherlandsMap = ({ height, width, mapData, plusButton }: Props) => {
 
         map.setView(center, mapZoom);
 
-        // L.tileLayer
-        //     .wms("https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0", { layers: "Kadastralekaart", minZoom: 17, maxNativeZoom: 22, maxZoom: 23 })
-        //     .addTo(map);
+        L.tileLayer
+            .wms("https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0", { layers: "Kadastralekaart", minZoom: 17, maxNativeZoom: 22, maxZoom: 23 })
+            .addTo(map);
 
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
             maxZoom: 17,
         }).addTo(map);
-
-        // selectedPlotLayer.addData(geojson as GeoJSON.GeoJsonObject);
-
         // Initialize Leaflet.draw control
-        // const drawnItems = new L.FeatureGroup();
-        // map.addLayer(drawnItems);
+        const drawnItems = new L.FeatureGroup();
+        map.addLayer(drawnItems);
 
-        // const drawControl = new L.Control.Draw({
-        //     edit: {
-        //         featureGroup: drawnItems,
-        //     },
-        // });
-        // map.addControl(drawControl);
+        const drawControl = new L.Control.Draw({
+            edit: {
+                featureGroup: drawnItems,
+            },
+        });
+        map.addControl(drawControl);
 
         // Handle draw events
-        // map.on(L.Draw.Event.CREATED, function (event) {
-        //     const layer = event.layer;
-        //     drawnItems.addLayer(layer);
-        // });
-
-        map.on("click", async (event) => {
-            // const crs = L.CRS.EPSG4326;
-            console.log(event.latlng);
-            // event.latlng.lat
-
-            // const bbox = map.getBounds().toBBoxString();
-            // console.log(bbox);
-            // const bbox = bounds.toBBoxString();
-            // const bounds = map.getBounds();
-            // const bbox = `${nw.x},${nw.y},${se.x},${se.y}`;
-
-            const loc = crs.project(event.latlng);
-
-            const bboxSize = 1;
-            const bbox = `${loc.x - bboxSize},${loc.y - bboxSize},${loc.x + bboxSize},${loc.y + bboxSize}`;
-            console.log(bbox);
-            const url = queryString.stringifyUrl({
-                url: baseUrlKadasterWms,
-                query: {
-                    QUERY_LAYERS: "Perceel",
-                    INFO_FORMAT: "application/json",
-                    REQUEST: "GetFeatureInfo",
-                    SERVICE: "WMS",
-                    VERSION: "1.3.0",
-                    HEIGHT: "101", // What?
-                    WIDTH: "101", // What?
-                    I: "50", // What?
-                    J: "50", // What?
-                    layers: "Perceel",
-                    CRS: crs.code,
-                    BBOX: bbox,
-                },
-            });
-
-            const res = await fetch(url);
-            const result = await res.json();
-            delete result.crs;
-            result.features[0].coordinates[0].map((c: [number, number]) => {
-                return crs.unproject(c);
-            });
-            L.geoJSON(result, {
-                style: {
-                    fillColor: "#FF00FF",
-                    fillOpacity: 1,
-                    color: "#B04173",
-                },
-            }).addTo(map);
-            console.log("on click", result);
+        map.on(L.Draw.Event.CREATED, function (event) {
+            const layer = event.layer;
+            drawnItems.addLayer(layer);
         });
+
         //Get this from backend
         mapData.map((data) => {
             return L.marker(data.coordinate, { icon: createMarkerIcon(data.projectColor) })
@@ -139,120 +84,3 @@ const NetherlandsMap = ({ height, width, mapData, plusButton }: Props) => {
 };
 
 export default NetherlandsMap;
-
-async function fetchStuff() {
-    fetchCapabilityWms();
-    fetchFeatureInfoWms3857();
-    // fetchFeatureInfoWms();
-    fetchFeatureInfoWms4326();
-}
-
-const baseUrlKadasterWms = "https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0";
-
-async function fetchCapabilityWms() {
-    const url = queryString.stringifyUrl({
-        url: baseUrlKadasterWms,
-        query: {
-            REQUEST: "GetCapabilities",
-            SERVICE: "WMS",
-        },
-    });
-
-    const res = await fetch(url);
-    const result = await res.text();
-    console.log("Capabilities", result);
-}
-
-async function fetchFeatureInfoWms4326() {
-    const lat = 571179.1781929513 / 100_000;
-    const lng = 6816654.835548575 / 100_000;
-    const lat2 = 571193.89834407 / 100_000;
-    const lng2 = 6816669.697605649 / 100_000;
-    const bbox = `${lat},${lng},${lat2},${lng2}`;
-    const url = queryString.stringifyUrl({
-        url: baseUrlKadasterWms,
-        query: {
-            QUERY_LAYERS: "Perceel",
-            INFO_FORMAT: "application/json",
-            REQUEST: "GetFeatureInfo",
-            SERVICE: "WMS",
-            VERSION: "1.3.0",
-            HEIGHT: "101", // What?
-            WIDTH: "101", // What?
-            I: "50", // What?
-            J: "50", // What?
-            layers: "Perceel",
-            // CRS: "EPSG:28992", // RD coords
-            CRS: "EPSG:4326",
-            BBOX: bbox,
-        },
-    });
-
-    const res = await fetch(url);
-    const result = await res.text();
-    console.log("EPSG:4326", result);
-}
-
-async function fetchFeatureInfoWms3857() {
-    const crs = "EPSG:3857";
-    const lat = 571179.1781929513; /// 100_000;
-    const lng = 6816654.835548575; /// 100_000;
-    const lat2 = 571193.89834407; /// 100_000;
-    const lng2 = 6816669.697605649; /// 100_000;
-    const bbox = `${lat},${lng},${lat2},${lng2}`;
-    const url = queryString.stringifyUrl({
-        url: baseUrlKadasterWms,
-        query: {
-            QUERY_LAYERS: "Perceel",
-            INFO_FORMAT: "application/json",
-            REQUEST: "GetFeatureInfo",
-            SERVICE: "WMS",
-            VERSION: "1.3.0",
-            HEIGHT: "101", // What?
-            WIDTH: "101", // What?
-            I: "50", // What?
-            J: "50", // What?
-            layers: "Perceel",
-            // CRS: "EPSG:28992", // RD coords
-            CRS: crs,
-            BBOX: bbox,
-        },
-    });
-
-    const res = await fetch(url);
-    const result = await res.text();
-    console.log("EPSG:3857", result);
-}
-
-// QUERY_LAYERS
-// 	Perceel
-// INFO_FORMAT
-// 	application/json
-// REQUEST
-// 	GetFeatureInfo
-// SERVICE
-// 	WMS
-// VERSION
-// 	1.3.0
-// FORMAT
-// 	image/png
-// STYLES
-
-// TRANSPARENT
-// 	true
-// layers
-// 	Perceel
-// FEATURE_COUNT
-// 	8
-// I
-// 	50
-// J
-// 	50
-// WIDTH
-// 	101
-// HEIGHT
-// 	101
-// CRS
-// 	EPSG:28992
-// BBOX
-// 	137441.18940484137,455927.1762410904,137450.28376863306,455936.27060488204
