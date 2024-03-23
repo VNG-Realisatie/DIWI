3f8e235de3668cbdc56140fc093d278cfe65a2a4
Emiel Rombouts <erombouts@phinion.com>
2024-03-23 10:12:57 +0100

    Make custom properties widget independent of layout
    .

---
+++ frontend/src/api/customProps.ts
@@ -0,0 +1,4 @@
+import { components } from "../types/schema";
+
+export type CustomPropDefinitions = components["schemas"]["CustomPropertyModel"];
+export type CustomPropertyValue = components["schemas"]["ProjectHouseblockCustomPropertyModel"];
