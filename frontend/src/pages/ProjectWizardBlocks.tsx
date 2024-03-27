8d7f6d0fe2efa7beb6861a6daeddb8c7baca13d1
Daniil Kravchenko <dkravchenko@phinion.com>
2024-03-27 15:51:44 +0100

    fixes saving house blocks, adds alerts

--- frontend/src/pages/ProjectWizardBlocks.tsx
+++ frontend/src/pages/ProjectWizardBlocks.tsx
@@ -1,46 +1,60 @@
-import { useId, useState } from "react";
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import usePlotSelector from "../hooks/usePlotSelector";
 import { HouseBlock } from "../components/project-wizard/house-blocks/types";
 import { emptyHouseBlockForm } from "../components/project-wizard/house-blocks/constants";
 import { addHouseBlock } from "../api/projectsServices";
 import { projectWizardMap, projectWizardWithId } from "../Paths";
 import WizardLayout from "../components/project-wizard/WizardLayout";
 import { HouseBlocksForm } from "../components/HouseBlocksForm";
+import useAlert from "../hooks/useAlert";
+import { useTranslation } from "react-i18next";
 
 const ProjectWizardBlocks = () => {
     const [createFormHouseBlock, setCreateFormHouseBlock] = useState<HouseBlock>(emptyHouseBlockForm);
     const [validationError, setValidationError] = useState("");
-
     const { projectId } = useParams();
     const navigate = useNavigate();
-    const id = useId();
-    const { handleSaveChange } = usePlotSelector(id);
+    const { setAlert } = useAlert();
+    const { t } = useTranslation();
 
     const handleNext = async () => {
-        if (!createFormHouseBlock.houseblockName) {
-            setValidationError("houseblockName");
-            return;
-        } else if (!createFormHouseBlock.startDate) {
-            setValidationError("startDate");
-            return;
-        } else if (!createFormHouseBlock.endDate) {
-            setValidationError("endDate");
-            return;
-        } else if (createFormHouseBlock.ownershipValue.some((owner) => owner.amount === null || isNaN(owner.amount))) {
-            setValidationError("value");
-            return;
-        }
-        await addHouseBlock({ ...createFormHouseBlock, projectId });
+        try {
+            if (!createFormHouseBlock.houseblockName) {
+                setValidationError("houseblockName");
+                return;
+            } else if (!createFormHouseBlock.startDate) {
+                setValidationError("startDate");
+                return;
+            } else if (!createFormHouseBlock.endDate) {
+                setValidationError("endDate");
+                return;
+            } else if (createFormHouseBlock.ownershipValue.some((owner) => owner.amount === null || isNaN(owner.amount))) {
+                setValidationError("value");
+                return;
+            }
+            await addHouseBlock({ ...createFormHouseBlock, projectId });
 
-        navigate(projectWizardMap.toPath({ projectId }));
+            navigate(projectWizardMap.toPath({ projectId }));
+        } catch (error: any) {
+            setAlert(error.message, "warning");
+        }
+    };
+
+    const handleSave = async () => {
+        try {
+            await addHouseBlock({ ...createFormHouseBlock, projectId });
+            setAlert(t("createProject.houseBlocksForm.notifications.successfullySaved"), "success");
+        } catch (error: any) {
+            setAlert(error.message, "warning");
+        }
     };
 
     const handleBack = () => {
         navigate(projectWizardWithId.toPath({ projectId }));
     };
+
     return (
-        <WizardLayout {...{ handleBack, handleNext, handleSave: handleSaveChange, projectId, activeStep: 1 }}>
+        <WizardLayout {...{ handleBack, handleNext, handleSave, projectId, activeStep: 1 }}>
             <HouseBlocksForm
                 validationError={validationError}
                 editForm={false}
