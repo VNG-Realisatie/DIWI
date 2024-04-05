c1e73e4ab8cf69d01e235e72228807d76cf38725
Lennart Schadron <lschadron@phinion.com>
2024-04-05 12:02:27 +0200

    Make clicking button refresh list

--- frontend/src/components/HouseBlocksFormWithControls.tsx
+++ frontend/src/components/HouseBlocksFormWithControls.tsx
@@ -1,6 +1,6 @@
 import { Box, Stack, Tooltip } from "@mui/material";
 import { HouseBlock } from "./project-wizard/house-blocks/types";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import SaveIcon from "@mui/icons-material/Save";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -8,6 +8,7 @@ import { t } from "i18next";
 import { HouseBlocksForm } from "./HouseBlocksForm";
 import { DeleteButtonWithConfirm } from "./DeleteButtonWithConfirm";
 import { deleteHouseBlock } from "../api/projectsServices";
+import HouseBlockContext from "../context/HouseBlockContext";
 
 type Props = {
     houseBlock: HouseBlock;
@@ -15,6 +16,8 @@ type Props = {
 };
 
 export const HouseBlocksFormWithControls = ({ houseBlock, setHouseBlock }: Props) => {
+    const { refresh } = useContext(HouseBlockContext);
+
     const [readOnly, setReadOnly] = useState(true);
     const [newHouseBlock, setNewHouseBlock] = useState<HouseBlock>(houseBlock);
 
@@ -37,7 +40,7 @@ export const HouseBlocksFormWithControls = ({ houseBlock, setHouseBlock }: Props
                         <DeleteButtonWithConfirm
                             typeAndName={`${t("generic.houseblock")} ${houseBlock.houseblockName}`}
                             deleteFunction={() => deleteHouseBlock(houseBlock.houseblockId ?? null)}
-                            afterDelete={undefined}
+                            afterDelete={refresh}
                         />
                     )}
                 </Stack>
