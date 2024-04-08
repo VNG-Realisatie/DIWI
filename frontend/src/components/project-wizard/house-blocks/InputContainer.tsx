9648eb7cd9cde18bc4218b325f578dbecf663b34
Emiel Rombouts <erombouts@phinion.com>
2024-04-08 09:54:31 +0200

    Use theme to set border radius of input container

--- frontend/src/components/project-wizard/house-blocks/InputContainer.tsx
+++ frontend/src/components/project-wizard/house-blocks/InputContainer.tsx
@@ -1,11 +1,13 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { ReactNode } from "react";
 interface Props {
     children: ReactNode;
 }
 export const InputContainer = ({ children }: Props) => {
+    const theme = useTheme();
+
     return (
-        <Box p={1} border="solid 1px #DDD" width="100%" height="100%">
+        <Box p={1} border="solid 1px #DDD" width="100%" height="100%" sx={{ borderRadius: theme.shape.borderRadius + "px" }}>
             {children}
         </Box>
     );
