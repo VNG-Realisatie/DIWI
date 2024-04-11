f3bd9adddc8fc62d78b9b21ec2a30be5347ff04a
Daniela Bazacliu <dbazacliu@phinion.com>
2024-04-11 10:32:58 +0300

    Validate houseblock mutation amount to be positive

--- backend/src/main/java/nl/vng/diwi/models/HouseblockSnapshotModel.java
+++ backend/src/main/java/nl/vng/diwi/models/HouseblockSnapshotModel.java
@@ -104,6 +104,8 @@ public class HouseblockSnapshotModel extends DatedDataModelSuperClass {
             return "Houseblock start date must be within the duration of the project.";
         } else if (projectEndDate.isBefore(this.getEndDate())) {
             return "Houseblock end date must be within the duration of the project";
+        } else if (mutation != null && mutation.getAmount() != null && mutation.getAmount() <= 0) {
+            return "Houseblock mutation amount must be greater than 0";
         }
 
         if (size != null) {
