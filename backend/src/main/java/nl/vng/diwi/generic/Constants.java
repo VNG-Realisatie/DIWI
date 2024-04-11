5dec48d5b5dd9fb3c1cfb64866d881a9525d58b5
Daniela Bazacliu <dbazacliu@phinion.com>
2024-04-11 10:42:05 +0300

    Fixes

---
+++ backend/src/main/java/nl/vng/diwi/generic/Constants.java
@@ -0,0 +1,7 @@
+package nl.vng.diwi.generic;
+
+public class Constants {
+
+    public static final String FIXED_PROPERTY_TARGET_GROUP = "targetGroup";
+    public static final String FIXED_PROPERTY_PHYSICAL_APPEARANCE = "physicalAppearance";
+}
