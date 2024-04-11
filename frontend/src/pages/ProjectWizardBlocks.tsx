99fb10d05c2ce2ee39c2b64e725cecb0c7889bc2
Daniil Kravchenko <dkravchenko@phinion.com>
2024-04-11 12:15:14 +0200

    enables deleting houseblocks

--- frontend/src/pages/ProjectWizardBlocks.tsx
+++ frontend/src/pages/ProjectWizardBlocks.tsx
@@ -6,11 +6,12 @@ import WizardLayout from "../components/project-wizard/WizardLayout";
 import { HouseBlocksForm } from "../components/HouseBlocksForm";
 import useAlert from "../hooks/useAlert";
 import HouseBlockContext from "../context/HouseBlockContext";
-import { t } from "i18next";
+import { useTranslation } from "react-i18next";
 import { Accordion, AccordionDetails, AccordionSummary, IconButton, Stack } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { deleteHouseBlock } from "../api/projectsServices";
 
 const ProjectWizardBlocks = () => {
     const { houseBlocks, addHouseBlock, getEmptyHouseBlock, updateHouseBlock, refresh } = useContext(HouseBlockContext);
@@ -20,6 +21,9 @@ const ProjectWizardBlocks = () => {
     const { setAlert } = useAlert();
     const [canUpdate, setCanUpdate] = useState(true);
     const [expanded, setExpanded] = useState(Array.from({ length: houseBlocksState.length }, () => true));
+    const [errorOccurred, setErrorOccurred] = useState(false);
+
+    const { t } = useTranslation();
 
     useEffect(() => {
         if (houseBlocks.length > 0) {
@@ -28,27 +32,37 @@ const ProjectWizardBlocks = () => {
     }, [houseBlocks]);
 
     const handleNext = async () => {
-        houseBlocksState.forEach((houseBlock) => {
-            if (
-                !houseBlock.houseblockName ||
-                !houseBlock.startDate ||
-                !houseBlock.endDate ||
-                houseBlock.ownershipValue.some((owner) => owner.amount === null || isNaN(owner.amount))
-            ) {
-                setAlert(t("createProject.hasMissingRequiredAreas.hasmissingProperty"), "warning");
-            } else {
-                if (houseBlock.houseblockId) {
-                    updateHouseBlock(houseBlock);
-                } else {
-                    addHouseBlock(houseBlock);
-                }
-                if (projectId) {
-                    navigate(projectWizardMap.toPath({ projectId }));
-                }
-            }
-        });
-    };
+        try {
+            setErrorOccurred(false);
 
+            await Promise.all(
+                houseBlocksState.map(async (houseBlock) => {
+                    if (
+                        !houseBlock.houseblockName ||
+                        !houseBlock.startDate ||
+                        !houseBlock.endDate ||
+                        houseBlock.ownershipValue.some((owner) => owner.amount === null || isNaN(owner.amount))
+                    ) {
+                        setErrorOccurred(true);
+                        throw new Error(t("createProject.hasMissingRequiredAreas.hasmissingProperty"));
+                    } else {
+                        if (houseBlock.houseblockId) {
+                            return updateHouseBlock(houseBlock);
+                        } else {
+                            return addHouseBlock(houseBlock);
+                        }
+                    }
+                }),
+            );
+
+            if (!errorOccurred && projectId) {
+                navigate(projectWizardMap.toPath({ projectId }));
+            }
+        } catch (error: any) {
+            setErrorOccurred(true);
+            setAlert(error.message, "warning");
+        }
+    };
     const handleSave = async () => {
         setCanUpdate(false);
         await Promise.all(
@@ -77,10 +91,15 @@ const ProjectWizardBlocks = () => {
     };
 
     useEffect(() => {
-        if (houseBlocks.length > 0 && canUpdate === true) {
-            setHouseBlocksState(houseBlocks);
+        if (houseBlocks.length > 0 && canUpdate) {
+            const filteredHouseBlocksState = houseBlocksState.filter(
+                (stateBlock) => !houseBlocks.some((block) => block.houseblockName === stateBlock.houseblockName),
+            );
+            const updatedHouseBlocksState = [...houseBlocks, ...filteredHouseBlocksState];
+
+            setHouseBlocksState(updatedHouseBlocksState);
         }
-    }, [houseBlocks, setHouseBlocksState, canUpdate]);
+    }, [houseBlocks, houseBlocksState, setHouseBlocksState, canUpdate]);
 
     const infoText = t("createProject.houseBlocksForm.info");
 
@@ -92,8 +111,19 @@ const ProjectWizardBlocks = () => {
         });
     };
 
-    const handleDeleteHouseBlock = (index: number) => {
-        // Implement delete logic here
+    const handleDeleteHouseBlock = async (index: number, houseblockId?: string) => {
+        try {
+            if (houseblockId) {
+                await deleteHouseBlock(houseblockId);
+                setAlert("deleted successfully", "success");
+                setHouseBlocksState((prevHouseBlocks) => prevHouseBlocks.filter((block) => block.houseblockId !== houseblockId));
+            } else {
+                setHouseBlocksState((prevHouseBlocks) => prevHouseBlocks.filter((_, i) => i !== index));
+            }
+        } catch (error) {
+            console.error("Error deleting house block:", error);
+            setAlert(t("errors.deleteHouseBlockError"), "error");
+        }
     };
 
     return (
@@ -106,15 +136,20 @@ const ProjectWizardBlocks = () => {
                         aria-controls="panel1-content"
                         id="panel1-header"
                     >
-                        <IconButton
-                            sx={{ marginLeft: "auto" }}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                handleDeleteHouseBlock(index);
-                            }}
-                        >
-                            <DeleteIcon sx={{ color: "#ffffff" }} />
-                        </IconButton>
+                        {houseBlock.houseblockName
+                            ? `${houseBlock.houseblockName}: ${houseBlock.mutation.grossPlanCapacity} ${t("createProject.houseBlocksForm.housesOn")} ${houseBlock.endDate}`
+                            : `${t("generic.houseblock")} ${index + 1}`}
+                        {houseBlocksState.length > 1 && (
+                            <IconButton
+                                sx={{ marginLeft: "auto" }}
+                                onClick={(event) => {
+                                    event.stopPropagation();
+                                    handleDeleteHouseBlock(index, houseBlock.houseblockId);
+                                }}
+                            >
+                                <DeleteIcon sx={{ color: "#ffffff" }} />
+                            </IconButton>
+                        )}
                     </AccordionSummary>
                     <AccordionDetails>
                         <HouseBlocksForm
