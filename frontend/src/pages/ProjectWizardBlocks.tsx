023288e418d5fe6ed17f72e4ff4e1c81612fb1f3
Julian <jvink@phinion.com>
2024-04-23 15:29:31 +0200

    Changes some design stuff in project details houseblocks

--- frontend/src/pages/ProjectWizardBlocks.tsx
+++ frontend/src/pages/ProjectWizardBlocks.tsx
@@ -237,13 +237,7 @@ const ProjectWizardBlocks = () => {
             {houseBlocksState.map((houseBlock, index) => (
                 <React.Fragment key={houseBlock.houseblockId ?? houseBlock.tempId}>
                     <Box ref={index === houseBlocksState.length - 1 ? lastAddedForm : null}>
-                        <Accordion
-                            expanded={expanded[index]}
-                            onChange={() => handleAccordionChange(index)}
-                            sx={{ width: "100%" }}
-                            disableGutters
-                            defaultExpanded
-                        >
+                        <Accordion expanded={expanded[index]} onChange={() => handleAccordionChange(index)} sx={{ width: "100%" }} disableGutters>
                             <AccordionSummary
                                 sx={{ backgroundColor: "#00A9F3", color: "#ffffff" }}
                                 expandIcon={<ExpandMoreIcon sx={{ color: "#ffffff" }} />}
