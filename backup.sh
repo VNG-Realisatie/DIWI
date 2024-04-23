ceb7f2873a9e39ab62661bd9c67a6a53a0de4901
Emiel Rombouts <erombouts@phinion.com>
2024-04-23 08:15:50 +0200

    Check if database is running before backing up
    Also log git hash for easier rollback to specific version

--- backup.sh
+++ backup.sh
@@ -20,7 +20,16 @@ DIWI_DB_NAME=${DIWI_DB_NAME:-diwi}
 DIWI_DB_USERNAME=${DIWI_DB_USERNAME:-diwi}
 
 timestamp=$(date --iso-8601=seconds)
-docker compose exec -T database pg_dump -U "$DIWI_DB_USERNAME" -d "$DIWI_DB_NAME" --format=c > "backup/predeploy-$timestamp.dump"
+
+# Check if databse container is running
+if  docker compose ps | grep -q database; then
+    docker compose exec -T database pg_dump -U "$DIWI_DB_USERNAME" -d "$DIWI_DB_NAME" --format=c > "backup/predeploy-$timestamp.dump"
+else
+    echo "Database container is not running, backup skipped"
+fi
+
+# Backup git hash so we can rollback to the specific version
+git rev-parse HEAD > "backup/predeploy-$timestamp.githash"
 
 # prune old backups
 find backup -type f -mtime +7 -delete
