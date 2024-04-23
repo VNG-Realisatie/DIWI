dbc9c44a3fb585d68a45fadc6c403b1d3495f100
Lennart Schadron <lschadron@phinion.com>
2024-04-23 13:59:57 +0200

    Fix frontend naming/data to upload

--- frontend/src/api/importServices.ts
+++ frontend/src/api/importServices.ts
@@ -2,7 +2,8 @@ import { API_URI } from "../utils/urls";
 
 export const importExcelProjects = async (file: FileList) => {
     const formData = new FormData();
-    Array.from(file).forEach((f) => formData.append("file", f));
+    //Array.from(file).forEach((f) => formData.append("file", f));
+    formData.append("uploadFile", file[0]);
 
     const response = await fetch(`${API_URI}/projects/import`, {
         method: "POST",
