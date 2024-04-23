2d5456046ed5b0d0f646cbcc8828f557b5e9673c
Lennart Schadron <lschadron@phinion.com>
2024-04-23 15:27:34 +0200

    Simplify clicking to upload

--- frontend/src/pages/ImportExcel.tsx
+++ frontend/src/pages/ImportExcel.tsx
@@ -1,7 +1,7 @@
 import { Button, Stack, Typography } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
 import { ReactComponent as UploadCloud } from "../assets/uploadCloud.svg";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useAlert from "../hooks/useAlert";
 import { useNavigate } from "react-router-dom";
 import * as Paths from "../Paths";
@@ -11,9 +11,17 @@ type Props = {
     excelImport: boolean;
 };
 export const ImportExcel = ({ excelImport }: Props) => {
+    const fileInputRef = useRef(null);
     const [uploaded, setUploaded] = useState(false);
     const { setAlert } = useAlert();
     const navigate = useNavigate();
+
+    function handleUploadStackClick(): void {
+        if (fileInputRef.current) {
+            (fileInputRef.current as HTMLElement).click();
+        }
+    }
+
     return (
         <Stack border="solid 1px #ddd" py={3} px={15}>
             <Typography fontSize="20px" fontWeight="600" sx={{ mt: 2 }}>
@@ -32,33 +40,41 @@ export const ImportExcel = ({ excelImport }: Props) => {
                 {excelImport ? "Upload ingevulde Excel template." : "Upload ingevulde Squit template."}
             </Typography>
             {!uploaded && (
-                <Stack mt={2} height={180} width="100%" border="dashed 2px #ddd" direction="column" alignItems="center" justifyContent="space-evenly">
-                    <label htmlFor="file-input" style={{ cursor: "pointer" }}>
-                        <UploadCloud />
-                        <input
-                            hidden
-                            id="file-input"
-                            type="file"
-                            onChange={(e) => {
-                                const file = e.target.files;
-                                if (file) {
-                                    importExcelProjects(file as FileList)
-                                        .then((res) => {
-                                            console.log(res);
-                                            console.log("File uploaded:", file);
-                                            setUploaded(true);
-                                            setAlert("Excel-bestand succesvol geüpload.", "success");
-                                            navigate(Paths.importExcelProjects.path);
-                                        })
-                                        .catch((error) => {
-                                            console.error("File upload failed:", error);
-                                            setAlert("Er is een fout opgetreden bij het uploaden van het bestand.", "error");
-                                        });
-                                }
-                            }}
-                        />
-                    </label>
-                    Sleep bestanden hierheen of klik om te uploaden
+                <Stack
+                    mt={2}
+                    height={180}
+                    width="100%"
+                    border="dashed 2px #ddd"
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="space-evenly"
+                    style={{ cursor: "pointer" }}
+                    onClick={handleUploadStackClick}
+                >
+                    <UploadCloud />
+                    <input
+                        hidden
+                        ref={fileInputRef}
+                        type="file"
+                        onChange={(e) => {
+                            const file = e.target.files;
+                            if (file) {
+                                importExcelProjects(file as FileList)
+                                    .then((res) => {
+                                        console.log(res);
+                                        console.log("File uploaded:", file);
+                                        setUploaded(true);
+                                        setAlert("Excel-bestand succesvol geüpload.", "success");
+                                        navigate(Paths.importExcelProjects.path);
+                                    })
+                                    .catch((error) => {
+                                        console.error("File upload failed:", error);
+                                        setAlert("Er is een fout opgetreden bij het uploaden van het bestand.", "error");
+                                    });
+                            }
+                        }}
+                    />
+                    Klik hier om te uploaden
                 </Stack>
             )}
         </Stack>
