8736f24f1725849149862a07145b08c3f5966489
Lennart Schadron <lschadron@phinion.com>
2024-05-07 09:45:36 +0200

    Fix indent using tabs to spaces

--- backend/src/main/java/nl/vng/diwi/security/UserRole.java
+++ backend/src/main/java/nl/vng/diwi/security/UserRole.java
@@ -2,12 +2,12 @@ package nl.vng.diwi.security;
 
 public enum UserRole {
     Admin(SecurityRoleConstants.Admin),
-	UserPlus(SecurityRoleConstants.UserPlus),
-	User(SecurityRoleConstants.User),
-	Management(SecurityRoleConstants.Management),
-	Council(SecurityRoleConstants.Council),
-	External(SecurityRoleConstants.External);
-
+    UserPlus(SecurityRoleConstants.UserPlus),
+    User(SecurityRoleConstants.User),
+    Management(SecurityRoleConstants.Management),
+    Council(SecurityRoleConstants.Council),
+    External(SecurityRoleConstants.External);
+    
     public final String name;
 
     UserRole(String name) {
