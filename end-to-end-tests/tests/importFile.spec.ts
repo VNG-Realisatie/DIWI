2d00485799bad3ad2a8630a1491970574bfbfcb4
Ali D <ali@phinion.com>
2024-05-10 10:02:49 +0200

    Remove unused os import

--- end-to-end-tests/tests/importFile.spec.ts
+++ end-to-end-tests/tests/importFile.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from "@playwright/test";
 import { PageManager } from "../page-objects/pageManager";
-import * as os from "os";
 
 test.beforeEach(async ({ page }) => {
     const pm = new PageManager(page);
