95b05282799502776eb1b3972168d307395b1bc7
Lennart Schadron <lschadron@phinion.com>
2024-05-29 13:06:50 +0200

    Add forbidden and userinfo paths

--- frontend/src/Paths.ts
+++ frontend/src/Paths.ts
@@ -41,6 +41,9 @@ export const loggedIn = createPathObject(config.baseurl + "rest/auth/loggedIn");
 export const logout = createPathObject(config.baseurl + "rest/auth/logout");
 export const home = createPathObject(config.baseurl + "home");
 export const about = createPathObject(config.baseurl + "about");
+export const forbidden = createPathObject(config.baseurl + "forbidden");
+
+export const userInfo = createPathObject(config.baseurl + "users/userinfo");
 
 export const userSettings = createPathObject(config.baseurl + "admin/settings");
 
