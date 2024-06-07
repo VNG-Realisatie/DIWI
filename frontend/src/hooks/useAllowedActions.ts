81295963d091ca5be0a54d118d04f8a0ecd4dcff
Daniil Kravchenko <dkravchenko@phinion.com>
2024-06-07 13:40:15 +0200

    disables hb update

--- frontend/src/hooks/useAllowedActions.ts
+++ frontend/src/hooks/useAllowedActions.ts
@@ -3,10 +3,10 @@ import { getCurrentUser } from "../api/userServices";
 
 export type AllowedActions =
     | "VIEW_API"
-    | "VIEW_USERS" //
-    | "EDIT_USERS" //
-    | "VIEW_GROUPS" //
-    | "EDIT_GROUPS" //
+    | "VIEW_USERS"
+    | "EDIT_USERS"
+    | "VIEW_GROUPS"
+    | "EDIT_GROUPS"
     | "VIEW_CONFIG"
     | "EDIT_CONFIG"
     | "VIEW_CUSTOM_PROPERTIES"
@@ -16,12 +16,11 @@ export type AllowedActions =
     | "VIEW_OWN_PROJECTS"
     | "EDIT_OWN_PROJECTS"
     | "CREATE_NEW_PROJECT"
-    | "IMPORT_PROJECTS" //
-    | "EXPORT_PROJECTS"; //
+    | "IMPORT_PROJECTS"
+    | "EXPORT_PROJECTS";
 
 function useAllowedActions() {
     const [allowedActions, setAllowedActions] = useState<AllowedActions[]>([]);
-    console.log(allowedActions);
 
     useEffect(() => {
         getCurrentUser().then((user) => {
