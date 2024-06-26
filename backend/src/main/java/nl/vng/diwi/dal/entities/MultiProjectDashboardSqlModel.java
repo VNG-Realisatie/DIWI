afecb458ed38dd1a1e5e878193094df1f946cf71
Emiel Rombouts <erombouts@phinion.com>
2024-06-26 16:42:36 +0200

    Remove comments

--- backend/src/main/java/nl/vng/diwi/dal/entities/MultiProjectDashboardSqlModel.java
+++ backend/src/main/java/nl/vng/diwi/dal/entities/MultiProjectDashboardSqlModel.java
@@ -1,36 +1,21 @@
 package nl.vng.diwi.dal.entities;
 
-import io.hypersistence.utils.hibernate.type.array.StringArrayType;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.Type;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
 import nl.vng.diwi.dal.JsonListType;
 import nl.vng.diwi.models.PieChartModel;
-import org.hibernate.annotations.Type;
-import org.hibernate.type.descriptor.java.StringJavaType;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Convert(
-   attributeName = "array",
-   converter = StringJavaType.class
-)
 @NoArgsConstructor
 @Getter
 @Setter
-@Log4j2
 public class MultiProjectDashboardSqlModel {
-
-    // MultiProjectDashboardSqlModel(Object input) {
-    //     log.info("input {}", input);
-    //     // this.multidimensional_array = multidimensional_array;
-    // }
-
     @Type(value = JsonListType.class)
     @Getter(AccessLevel.NONE)
     private List<PieChartModel> physicalAppearance;
