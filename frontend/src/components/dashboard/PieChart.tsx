d95d22d3fcafdf5f30fe18bb58cdb83e6010ee02
Ali D. <ali@phinion.com>
2024-06-27 14:37:45 +0200

    Create pie chart component

---
+++ frontend/src/components/dashboard/PieChart.tsx
@@ -0,0 +1,65 @@
+import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart";
+import { ChartType } from "../../pages/DashboardProject";
+import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
+import { LegendRendererProps } from "@mui/x-charts";
+const size = {
+    width: 500,
+    height: 300,
+};
+type Props = {
+    chartData: ChartType[];
+    colors: string[];
+};
+
+export const DashboardPieChart = ({ chartData, colors }: Props) => {
+    const theme = useTheme();
+
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+    const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+
+    const getLegendPosition = (): Partial<LegendRendererProps> | undefined => {
+        if (isSmallScreen) {
+            return {
+                direction: "row",
+                position: { vertical: "bottom", horizontal: "left" },
+                padding: 10,
+                itemGap: 10,
+                labelStyle: {
+                    fontSize: 12,
+                },
+            };
+        }
+        if (isLargeScreen) {
+            return {
+                direction: "column",
+                position: { vertical: "middle", horizontal: "right" },
+                padding: 0,
+            };
+        }
+    };
+
+    return (
+        <PieChart
+            colors={colors}
+            margin={isSmallScreen ? { top: 10, bottom: 70 } : { top: 10, bottom: 50, left: -100 }}
+            series={[
+                {
+                    arcLabel: (item) => `${item.value}`,
+                    arcLabelMinAngle: 10,
+                    data: chartData,
+                },
+            ]}
+            slotProps={{
+                legend: getLegendPosition(),
+            }}
+            sx={{
+                [`& .${pieArcLabelClasses.root}`]: {
+                    fill: "white",
+                    fontWeight: "bold",
+                },
+            }}
+            {...size}
+        />
+    );
+};
--- frontend/src/pages/DashboardProject.tsx
+++ frontend/src/pages/DashboardProject.tsx
@@ -1,15 +1,36 @@
 import { Grid, Stack, Typography } from "@mui/material";
 import BreadcrumbBar from "../components/header/BreadcrumbBar";
 import * as Paths from "../Paths";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import ProjectContext from "../context/ProjectContext";
 import { useParams } from "react-router-dom";
+import { DashboardPieChart } from "../components/dashboard/PieChart";
+import { getDashboardProject } from "../api/dashboardServices";
+
+export type ChartType = {
+    label: string;
+    value: number;
+};
 
 export const DashboardProject = () => {
     const { t } = useTranslation();
     const { selectedProject } = useContext(ProjectContext);
     const { projectId } = useParams();
+    const [physicalAppearance, setPhysicalAppearance] = useState<ChartType[]>([]);
+
+    const physicalAppearanceColor = ["#0D3B66", "#145DA0", "#1E7AC9", "#2A9DF4", "#63B2F5", "#8CC6F5", "#B5DAF7", "#D6EAF8", "#E9F5FB", "#F7FBFD"];
+
+    useEffect(() => {
+        if (projectId) {
+            getDashboardProject(projectId).then((data) => {
+                const convertedData = data.physicalAppearance.map((d) => {
+                    return { label: d.name, value: d.amount };
+                });
+                setPhysicalAppearance(convertedData);
+            });
+        }
+    }, [projectId]);
     return (
         <Stack>
             <BreadcrumbBar
@@ -30,6 +51,7 @@ export const DashboardProject = () => {
                     <Typography variant="h6" fontSize={16}>
                         {t("dashboard.priceSegmentsPurchase")}
                     </Typography>
+                    <DashboardPieChart chartData={physicalAppearance} colors={physicalAppearanceColor} />
                     {/* ToDo:Add chart here later */}
                 </Grid>
                 <Grid item xs={12} md={6}>
