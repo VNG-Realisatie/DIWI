90dd0045be77b2dd291a92a482bba43e1a60dc68
Emiel Rombouts <erombouts@phinion.com>
2024-07-03 13:34:23 +0200

    Add setup test to make sure test user is hoofd gebruiker

--- end-to-end-tests/playwright.config.ts
+++ end-to-end-tests/playwright.config.ts
@@ -33,8 +33,13 @@ export default defineConfig({
     /* Configure projects for major browsers */
     projects: [
         {
+            name: "setup user",
+            testMatch: /global\.setup\.ts/,
+        },
+        {
             name: "chromium",
             use: { ...devices["Desktop Chrome"] },
+            dependencies: ["setup user"],
         },
     ],
 });
