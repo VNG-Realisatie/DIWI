141a59ae4356982ea79114af8ec12b93c79988be
Daniil Kravchenko <dkravchenko@phinion.com>
2024-07-15 13:55:21 +0200

    appends variables to .env, ads vite prefix for docker file

--- version.sh
+++ version.sh
@@ -9,3 +9,18 @@ export VITE_REACT_APP_GIT_SHA
 
 VITE_REACT_APP_DEPLOY_DATE=$(date -Iseconds)
 export VITE_REACT_APP_DEPLOY_DATE
+
+# Function to replace or append variable
+replace_or_append() {
+  local file=".env"
+  local key="$1"
+  local value="$2"
+  if grep -q "^$key=" "$file"; then
+    sed -i "s/^$key=.*/$key=$value/" "$file"
+  else
+    echo "$key=$value" >> "$file"
+  fi
+}
+replace_or_append "VITE_REACT_APP_VERSION_NUMBER" "$VITE_REACT_APP_VERSION_NUMBER"
+replace_or_append "VITE_REACT_APP_GIT_SHA" "$VITE_REACT_APP_GIT_SHA"
+replace_or_append "VITE_REACT_APP_DEPLOY_DATE" "$VITE_REACT_APP_DEPLOY_DATE"
