463a7c189918043f4618d5ac93e2eca45a7fe06a
Ali D. <ali@phinion.com>
2024-08-14 11:14:58 +0200

    Fix import test

--- end-to-end-tests/tests/importFile.spec.ts
+++ end-to-end-tests/tests/importFile.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { PageManager } from "../page-objects/pageManager";
+import * as path from "path";
 
 test.beforeEach(async ({ page }) => {
     const pm = new PageManager(page);
@@ -8,23 +9,23 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("Import file success", async ({ page }) => {
-    const filePath = `excel-importer.xlsx`;
+    const filePath = `end-to-end-tests/excel-importer.xlsx`;
 
     await page.locator('input[type="file"]').setInputFiles(filePath);
     await expect(page).toHaveURL("http://localhost:3000/projects/table");
 });
 
 test("Import file failed", async ({ page }) => {
-    const filePath = `excel-false.xlsx`;
+    const filePath = `end-to-end-tests/excel-false.xlsx`;
 
     await page.locator('input[type="file"]').setInputFiles(filePath);
 
     const rowError = await page.locator(".rij p").innerText();
     const valueError = await page.locator(".waarde p").last().innerText();
-    const DescriptionError = await page.locator(".import-error").innerText();
+    const descriptionError = await page.locator(".import-error").innerText();
 
     expect(rowError).toContain("3");
 
     expect(valueError).toContain("Plan soort");
-    expect(DescriptionError).toContain("Het Excel-bestand bevat niet alle verwachte koppen.");
+    expect(descriptionError).toContain("Het Excel-bestand bevat niet alle verwachte koppen.");
 });
