a2cb4982bf1f91bf65334baf6eb82fe569a39e49
Julian <jvink@phinion.com>
2024-09-10 16:07:05 +0200

    Fixes wizard map features

--- frontend/src/components/map/ProjectPlotSelector.tsx
+++ frontend/src/components/map/ProjectPlotSelector.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import ClearIcon from "@mui/icons-material/Clear";
 import SaveIcon from "@mui/icons-material/Save";
 import Tooltip from "@mui/material/Tooltip";
@@ -14,45 +15,76 @@ import { Details } from "../Details";
 import { AddHouseBlockButton } from "../PlusButton";
 import { CreateHouseBlockDialog } from "../project/project-with-house-block/CreateHouseBlockDialog";
 import { useHasEditPermission } from "../../hooks/useHasEditPermission";
+import WizardLayout from "../project-wizard/WizardLayout";
+import { useNavigate, useParams } from "react-router-dom";
+import { projectDetailCharacteristics, projectWizardBlocks } from "../../Paths";
 
-const ProjectPlotSelector = () => {
+const ProjectPlotSelector = ({ wizard = false }) => {
     const { t } = useTranslation();
 
     const { selectedProject } = useContext(ProjectContext);
     const [openHouseBlockDialog, setOpenHouseBlockDialog] = useState(false);
     const id = useId();
-    const { plotsChanged, handleCancelChange, handleSaveChange, selectionMode, toggleSelectionMode } = usePlotSelector(id);
+    const { selectedPlotCount, plotsChanged, handleCancelChange, handleSaveChange, selectionMode, toggleSelectionMode } = usePlotSelector(id);
     const { getEditPermission } = useHasEditPermission();
 
     const handleAddHouseBlockClick = () => {
         setOpenHouseBlockDialog(true);
     };
 
-    return (
-        <Stack my={1} p={1} mb={10}>
-            <Box sx={{ cursor: "pointer" }} position="absolute" right={105} top={17}>
-                {plotsChanged && getEditPermission() && (
-                    <>
-                        <Tooltip placement="top" title={t("generic.cancelChanges")}>
-                            <ClearIcon sx={{ mr: 2, color: "#FFFFFF" }} onClick={handleCancelChange} />
-                        </Tooltip>
-                        <Tooltip placement="top" title={t("generic.saveChanges")}>
-                            <SaveIcon sx={{ color: "#FFFFFF" }} onClick={handleSaveChange} />
-                        </Tooltip>
-                    </>
-                )}
-            </Box>
+    const { projectId } = useParams();
+    const navigate = useNavigate();
+
+    const handleNext = async () => {
+        await handleSaveChange();
+        if (projectId) {
+            navigate(projectDetailCharacteristics.toPath({ projectId }));
+        }
+    };
+
+    const handleBack = async () => {
+        await handleSaveChange();
+        if (projectId) {
+            navigate(projectWizardBlocks.toPath({ projectId }));
+        }
+    };
+
+    const infoText = t("createProject.selectMapForm.info");
+    const warning = selectedPlotCount <= 0 ? t("createProject.selectMapForm.warning") : undefined;
+
+    const style = (
+        <Stack my={1} p={1} mb={0}>
+            {!wizard && (
+                <Box sx={{ cursor: "pointer" }} position="absolute" right={105} top={17}>
+                    {plotsChanged && getEditPermission() && (
+                        <>
+                            <Tooltip placement="top" title={t("generic.cancelChanges")}>
+                                <ClearIcon sx={{ mr: 2, color: "#FFFFFF" }} onClick={handleCancelChange} />
+                            </Tooltip>
+                            <Tooltip placement="top" title={t("generic.saveChanges")}>
+                                <SaveIcon sx={{ color: "#FFFFFF" }} onClick={handleSaveChange} />
+                            </Tooltip>
+                        </>
+                    )}
+                </Box>
+            )}
+
             <Stack direction="row" alignItems="center" justifyContent="space-between">
-                <Stack overflow="auto" height="70vh" width="20%">
-                    <Details project={selectedProject} />
-                </Stack>
-                <div id={id} style={{ height: "70vh", width: "100%", paddingLeft: 8 }}></div>
-                <div style={{ position: "absolute", bottom: 100, right: 20 }}>
-                    <AddHouseBlockButton onClick={handleAddHouseBlockClick} />
-                    <CreateHouseBlockDialog openHouseBlockDialog={openHouseBlockDialog} setOpenHouseBlockDialog={setOpenHouseBlockDialog} />
-                </div>
+                {!wizard && (
+                    <Stack overflow="auto" height="70vh" width="20%">
+                        <Details project={selectedProject} />
+                    </Stack>
+                )}
+                <div id={id} style={{ height: !wizard ? "70vh" : "60vh", width: "100%", paddingLeft: 8 }}></div>
+                {!wizard && (
+                    <div style={{ position: "absolute", bottom: 40, right: 20 }}>
+                        <AddHouseBlockButton onClick={handleAddHouseBlockClick} />
+                        <CreateHouseBlockDialog openHouseBlockDialog={openHouseBlockDialog} setOpenHouseBlockDialog={setOpenHouseBlockDialog} />
+                    </div>
+                )}
             </Stack>
-            <Box position="absolute" top={90} right={20}>
+
+            <Box position="absolute" top={!wizard ? 90 : 265} right={!wizard ? 20 : 150}>
                 {getEditPermission() && (
                     <>
                         <Tooltip title={selectionMode === Buttons.SELECT ? t("generic.cancelSelection") : t("generic.selectPlot")}>
@@ -119,6 +151,12 @@ const ProjectPlotSelector = () => {
             </Box>
         </Stack>
     );
+
+    if (wizard) {
+        return <WizardLayout {...{ infoText, warning, handleBack, handleNext, handleSave: handleSaveChange, projectId, activeStep: 2 }}>{style}</WizardLayout>;
+    } else if (!wizard) {
+        return <div>{style}</div>;
+    }
 };
 
 export default ProjectPlotSelector;
