c6491bd97738398d4fad9dfe9098fde059a940ee
Emiel Rombouts <erombouts@phinion.com>
2024-09-24 15:25:34 +0200

    Fix migration script

--- backend/src/main/resources/db/migration/V2024.09.10.14.00__FixPriceCategories.sql
+++ backend/src/main/resources/db/migration/V2024.09.10.14.00__FixPriceCategories.sql
@@ -34,7 +34,7 @@ SET
                 ELSE vs.max * 100
             END
         FROM
-            property_range_category_value_state AS vs2
+            diwi.property_range_category_value_state AS vs2
             INNER JOIN diwi.user AS u ON u.id = vs2.create_user_id
             INNER JOIN diwi.property_range_category_value AS prcv ON prcv.id = vs.range_category_value_id
             INNER JOIN diwi.property_state AS ps ON ps.property_id = prcv.property_id
