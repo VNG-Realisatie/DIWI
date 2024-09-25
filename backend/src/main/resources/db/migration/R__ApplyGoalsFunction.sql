659ed85f7bd15e65bd7f21a22dfb55fde71b53ad
Daniela Bazacliu <dbazacliu@phinion.com>
2024-09-25 23:29:40 +0300

    Count number of houses as negative amounts in the case of a destruction houseblock

--- backend/src/main/resources/db/migration/R__ApplyGoalsFunction.sql
+++ backend/src/main/resources/db/migration/R__ApplyGoalsFunction.sql
@@ -32,7 +32,7 @@ SELECT
     pgv.goal_value     AS goalValue,
     pgv.type        AS goalType,
     pgv.direction   AS goalDirection,
-    (SELECT COALESCE(SUM(hv.no_of_houses), 0)
+    (SELECT COALESCE(SUM(hv.no_of_houses * hv.mutation_sign), 0)
         FROM houseblocks_view hv
      WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
     )               AS totalAmount,
@@ -40,7 +40,7 @@ SELECT
         CASE
             WHEN pgv.property_category_options IS NOT NULL THEN
                 (
-                    SELECT COALESCE(SUM(no_of_houses), 0)
+                    SELECT COALESCE(SUM(no_of_houses * mutation_sign), 0)
                     FROM
                         houseblocks_view hv
                     WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
@@ -50,12 +50,12 @@ SELECT
                 (
                     WITH physical_appearance_values AS
                         (
-                            SELECT unnest(hv.physical_appearance) AS pa_val
+                            SELECT unnest(hv.physical_appearance) AS pa_val, hv.mutation_sign
                             FROM
                                 houseblocks_view hv
                             WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
                         )
-                    SELECT COALESCE ( SUM(cast (pav.pa_val->>'amount' AS integer)), 0)
+                    SELECT COALESCE ( SUM(cast (pav.pa_val->>'amount' AS integer) * pav.mutation_sign), 0)
                     FROM physical_appearance_values pav
                     WHERE (pav.pa_val->>'physical_appearance') = ANY (pgv.physical_appearance_options)
                 )
@@ -63,12 +63,12 @@ SELECT
                 (
                     WITH housetype_values AS
                         (
-                            SELECT unnest(hv.house_type) AS ht_val
+                            SELECT unnest(hv.house_type) AS ht_val, hv.mutation_sign
                             FROM
                                 houseblocks_view hv
                             WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
                         )
-                    SELECT COALESCE ( SUM(cast (htv.ht_val->>'amount' AS integer)), 0)
+                    SELECT COALESCE ( SUM(cast (htv.ht_val->>'amount' AS integer) * htv.mutation_sign), 0)
                     FROM housetype_values htv
                     WHERE (htv.ht_val->>'house_type') = ANY (pgv.house_type_options)
                 )
@@ -76,12 +76,12 @@ SELECT
                 (
                     WITH targetgroup_values AS
                         (
-                            SELECT unnest(hv.target_group) AS tg_val
+                            SELECT unnest(hv.target_group) AS tg_val, hv.mutation_sign
                             FROM
                                 houseblocks_view hv
                             WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
                         )
-                    SELECT COALESCE ( SUM(cast (tgv.tg_val->>'amount' AS integer)), 0)
+                    SELECT COALESCE ( SUM(cast (tgv.tg_val->>'amount' AS integer) * tgv.mutation_sign), 0)
                     FROM targetgroup_values tgv
                     WHERE (tgv.tg_val->>'target_group') = ANY (pgv.target_group_options)
                 )
@@ -89,18 +89,18 @@ SELECT
                 (
                     WITH groundposition_values AS
                         (
-                            SELECT unnest(hv.ground_position) AS ht_val
+                            SELECT unnest(hv.ground_position) AS ht_val, hv.mutation_sign
                             FROM
                                 houseblocks_view hv
                             WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
                         )
-                    SELECT COALESCE ( SUM(cast (gpv.ht_val->>'amount' AS integer)), 0)
+                    SELECT COALESCE ( SUM(cast (gpv.ht_val->>'amount' AS integer) * gpv.mutation_sign), 0)
                     FROM groundposition_values gpv
                     WHERE (gpv.ht_val->>'ground_position') = ANY (pgv.ground_position_options)
                 )
             WHEN pgv.programming IS NOT NULL THEN
                 (
-                    SELECT COALESCE(SUM(no_of_houses), 0)
+                    SELECT COALESCE(SUM(no_of_houses * mutation_sign), 0)
                     FROM
                         houseblocks_view hv
                     WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
@@ -108,7 +108,7 @@ SELECT
                 )
             WHEN pgv.property_boolean_option IS NOT NULL THEN
                 (
-                    SELECT COALESCE(SUM(no_of_houses), 0)
+                    SELECT COALESCE(SUM(no_of_houses * mutation_sign), 0)
                     FROM
                         houseblocks_view hv
                     WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
@@ -121,9 +121,10 @@ SELECT
                             SELECT
                                 hv_ownership.ht_val ->> 'amount' AS amount,
                                 hv_ownership.ht_val ->> 'property_value_id' AS property_value_id,
-                                hv_ownership.ht_val ->> 'ownership_type' AS ownership_type
+                                hv_ownership.ht_val ->> 'ownership_type' AS ownership_type,
+                                hv_ownership.mutation_sign AS mutation_sign
                             FROM
-                            ( SELECT unnest(hv.ownership_value_options) AS ht_val
+                            ( SELECT unnest(hv.ownership_value_options) AS ht_val, hv.mutation_sign
                                 FROM
                                     houseblocks_view hv
                                 WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
@@ -143,13 +144,13 @@ SELECT
                         )
 
                     SELECT
-                        COALESCE(SUM(cast (hov.amount AS integer)), 0)
+                        COALESCE(SUM(cast (hov.amount AS integer) * hov.mutation_sign), 0)
                     FROM
                         policy_goals_options pgo
                             JOIN houseblock_ownership_values hov ON pgo.property_value_id = hov.property_value_id AND pgo.ownership_type = hov.ownership_type
                 )
             ELSE (
-                SELECT COALESCE(SUM(hv.no_of_houses), 0)
+                SELECT COALESCE(SUM(hv.no_of_houses * hv.mutation_sign), 0)
                 FROM houseblocks_view hv
                 WHERE hv.delivery_date >= pgv.start_date AND hv.delivery_date <= pgv.end_date
             )
