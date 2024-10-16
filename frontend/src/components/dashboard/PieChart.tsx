67c862bf120f15ce8b40774866c91d1b8517118e
Daniil Kravchenko <dkravchenko@phinion.com>
2024-10-16 11:10:30 +0200

    filters out negative values, show total number on top and percentage on hover

--- frontend/src/components/dashboard/PieChart.tsx
+++ frontend/src/components/dashboard/PieChart.tsx
@@ -2,9 +2,10 @@ import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart";
 import { ChartType } from "../../pages/DashboardProject";
 import { useTheme } from "@mui/material/styles";
 import { Box, Stack, Typography, useMediaQuery } from "@mui/material";
-import { DefaultizedPieValueType } from "@mui/x-charts";
+import { DefaultizedPieValueType, PieValueType } from "@mui/x-charts";
 import { useTranslation } from "react-i18next";
 import { generateColorsArray } from "../../utils/dashboardChartColors";
+import { MakeOptional } from "@mui/x-charts/internals";
 
 type Props = {
     chartData: ChartType[];
@@ -21,18 +22,23 @@ export const DashboardPieChart = ({ chartData, isPdfChart }: Props) => {
         height: 300,
     };
 
+    const chartDataWithoutNegativeValues = chartData.filter((data) => data.value >= 0);
+
     const chartMargin = isSmallScreen && !isPdfChart ? { top: 10, bottom: 100, left: 50 } : { top: 10, bottom: 50, left: 10, right: 10 };
 
-    const TOTAL = chartData.map((item) => item.value).reduce((a, b) => a + b, 0);
+    const TOTAL = chartDataWithoutNegativeValues.map((item) => item.value).reduce((a, b) => a + b, 0);
 
     const getArcLabel = (params: DefaultizedPieValueType) => {
+        return `${params.value}`;
+    };
+
+    const getTooltipLabel = (params: MakeOptional<PieValueType, "id">) => {
         const percent = params.value / TOTAL;
         return `${(percent * 100).toFixed(0)}%`;
     };
-
     return (
         <>
-            {chartData.every((data) => data.value === 0) ? (
+            {chartData.every((data) => data.value <= 0) ? (
                 <Typography variant="h6" color="error" sx={{ textAlign: "center", mt: `${size.height / 2}px` }} {...size}>
                     {t("dashboard.chartData.noData")}
                 </Typography>
@@ -45,7 +51,8 @@ export const DashboardPieChart = ({ chartData, isPdfChart }: Props) => {
                             {
                                 arcLabel: (item) => `${getArcLabel(item)}`,
                                 arcLabelMinAngle: 24,
-                                data: chartData,
+                                data: chartDataWithoutNegativeValues,
+                                valueFormatter: (params: MakeOptional<PieValueType, "id">) => getTooltipLabel(params),
                             },
                         ]}
                         slotProps={{
@@ -60,7 +67,7 @@ export const DashboardPieChart = ({ chartData, isPdfChart }: Props) => {
                         {...size}
                     />
                     <Stack direction={isSmallScreen && !isPdfChart ? "row" : "column"} flexWrap="wrap" maxHeight={"260px"} width="100%">
-                        {chartData.map((data, i) => {
+                        {chartDataWithoutNegativeValues.map((data, i) => {
                             return (
                                 <Stack direction="row" sx={{ m: 1 }} key={i}>
                                     <Box height={20} width={20} sx={{ backgroundColor: generateColorsArray(chartData.length)[i] }}></Box>
