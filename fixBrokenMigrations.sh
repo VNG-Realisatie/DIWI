819c54a6fe542f0fe06e684fcc91c27d58b6b256
Emiel Rombouts <erombouts@phinion.com>
2024-12-23 16:25:29 +0100

    Be more tolerant when database is not running

--- fixBrokenMigrations.sh
+++ fixBrokenMigrations.sh
@@ -5,5 +5,6 @@
 
 set -eux
 
-docker compose exec -T database psql -U "$DIWI_DB_USERNAME" -d "$DIWI_DB_NAME" < database-fixes/FixV2024.09.10.14.00__FixPriceCategories.sql
-docker compose exec -T database psql -U "$DIWI_DB_USERNAME" -d "$DIWI_DB_NAME" < database-fixes/FixSystemUserMigrations.sql
+# This only works if the database is already running. Hence the || true at the end
+docker compose exec -T database psql -U "$DIWI_DB_USERNAME" -d "$DIWI_DB_NAME" < database-fixes/FixV2024.09.10.14.00__FixPriceCategories.sql || true
+docker compose exec -T database psql -U "$DIWI_DB_USERNAME" -d "$DIWI_DB_NAME" < database-fixes/FixSystemUserMigrations.sql || true
