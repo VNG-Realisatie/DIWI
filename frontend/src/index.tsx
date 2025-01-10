e188ecf47645cd21faa49035bd6766c5d3ad87e3
Emiel Rombouts <erombouts@phinion.com>
2025-01-10 12:27:07 +0100

    Re-enable strict mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,11 +9,12 @@ import { UserProvider } from "./context/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    // <React.StrictMode>
-    <UserProvider>
-        <App />
-    </UserProvider>,
-    // </React.StrictMode>,
+    <React.StrictMode>
+        <UserProvider>
+            <App />
+        </UserProvider>
+        ,
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
