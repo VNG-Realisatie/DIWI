73b5663dda3500b653b98c5fa53ba3cdc8b51d19
Vladimir Stanica <vstanica@phinion.com>
2025-02-14 12:47:21 +0200

    Added support for storing clientId-> Renamed sql script

--- backend/src/main/resources/db/migration/V2025.01.13.12.00__AddClientIdAndUserIdToDataExchangeState.sql
+++ backend/src/main/resources/db/migration/V2025.01.13.12.00__AddClientIdAndUserIdToDataExchangeState.sql
@@ -4,5 +4,3 @@ ALTER TABLE diwi.data_exchange_state
 ALTER TABLE diwi.data_exchange_state
     ADD COLUMN user_id uuid DEFAULT NULL;
 
-ALTER TABLE diwi.data_exchange_state
-    ADD CONSTRAINT unique_client_user UNIQUE (client_id, user_id);
