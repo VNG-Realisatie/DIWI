20ce3849c0be88697b133f6fbf1f31d8a91a080f
Vladimir Stanica <vstanica@phinion.com>
2025-02-26 12:51:15 +0200

    pzh export _ added logging for the validation errors + refactored DataExchangeExportError

--- backend/src/main/java/nl/vng/diwi/services/DataExchangeExportError.java
+++ backend/src/main/java/nl/vng/diwi/services/DataExchangeExportError.java
@@ -1,5 +1,6 @@
 package nl.vng.diwi.services;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import nl.vng.diwi.services.export.zuidholland.EsriZuidHollandHouseblockExportModel.OwnershipCategory;
@@ -8,13 +9,12 @@ import java.util.UUID;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class DataExchangeExportError {
 
     private UUID projectId;
     private UUID houseblockId;
-
     private String fieldName;
-
     private String code;
     private String message;
     private OwnershipCategory cat1;
@@ -22,49 +22,53 @@ public class DataExchangeExportError {
     private Long priceValueMin;
     private Long priceValueMax;
 
-    public DataExchangeExportError(UUID projectId, String fieldName, EXPORT_ERROR exportError) {
+    private DataExchangeExportError(UUID projectId, UUID houseblockId, String fieldName, EXPORT_ERROR exportError, OwnershipCategory cat1, OwnershipCategory cat2, Long priceValueMin, Long priceValueMax) {
         this.projectId = projectId;
+        this.houseblockId = houseblockId;
         this.fieldName = fieldName;
-        this.code = exportError.errorCode;
-        this.message = exportError.errorMsg;
-    }
-
-    public DataExchangeExportError(UUID projectId, UUID houseblockId, EXPORT_ERROR exportError) {
-        this.projectId = projectId;
-        this.houseblockId = houseblockId;
-        this.code = exportError.errorCode;
-        this.message = exportError.errorMsg;
-    }
-
-    public DataExchangeExportError(UUID projectId, UUID houseblockId, EXPORT_ERROR exportError, OwnershipCategory cat1, OwnershipCategory cat2, Long priceValueMin, Long priceValueMax) {
-        this.projectId = projectId;
-        this.houseblockId = houseblockId;
-        this.code = exportError.errorCode;
-        this.message = exportError.errorMsg;
-
+        this.code = exportError.getErrorCode();
+        this.message = exportError.getErrorMessage();
         this.cat1 = cat1;
         this.cat2 = cat2;
         this.priceValueMin = priceValueMin;
         this.priceValueMax = priceValueMax;
     }
 
-    public enum EXPORT_ERROR {
-
-        MISSING_MANDATORY_VALUE("missing_mandatory_value", "Value is mandatory in the export, but is missing from the project properties."),
-        MULTIPLE_SINGLE_SELECT_VALUES("multiple_single_select_values", "Value is single-select in the export, but multiple values were assigned in the project properties."),
-        VALUE_LARGER_THAN_CONSTRUCTION_HOUSEBLOCKS("value_larger_than_construciton_houseblocks", "Value is greater than the total number of houses in the project's construction houseblocks, which is not allowed."),
-        MISSING_DATAEXCHANGE_MAPPING("missing_dataexchange_mapping", "The dataexchange mapping is incomplete. A custom property is not configured for this field."),
-        NUMERIC_RANGE_VALUE("numeric_range_value", "The numeric property has a range value assigned for this project, but only single values are allowed."),
-        OWNERSHIP_RANGE_MAPPING_ERROR("ownership_range_mapping_error", "The ownership range selected cannot be mapped within one interval of the export ranges."),
-        CONFIDENTIALITY_ERROR("min_confidentiality_error", "The confidentiality level of the project is less than the minimum allowed export confidentiality");
-
-        public final String errorMsg;
-        public final String errorCode;
-
-        EXPORT_ERROR(String errorCode, String errorMsg) {
-            this.errorCode = errorCode;
-            this.errorMsg = errorMsg;
-        }
+    public DataExchangeExportError(UUID projectId, String fieldName, EXPORT_ERROR exportError) {
+        this(projectId, null, fieldName, exportError, null, null, null, null);
     }
 
+    public DataExchangeExportError(UUID projectId, UUID houseblockId, EXPORT_ERROR exportError) {
+        this(projectId, houseblockId, null, exportError, null, null, null, null);
+    }
+
+    public DataExchangeExportError(UUID projectId, UUID houseblockId, EXPORT_ERROR exportError, OwnershipCategory cat1, OwnershipCategory cat2, Long priceValueMin, Long priceValueMax) {
+        this(projectId, houseblockId, null, exportError, cat1, cat2, priceValueMin, priceValueMax);
+    }
+
+    public enum EXPORT_ERROR {
+        MISSING_MANDATORY_VALUE("missing_mandatory_value", "Value is mandatory in the export, but is missing from the project properties."),
+        MULTIPLE_SINGLE_SELECT_VALUES("multiple_single_select_values", "Value is single-select in the export, but multiple values were assigned in the project properties."),
+        VALUE_LARGER_THAN_CONSTRUCTION_HOUSEBLOCKS("value_larger_than_construction_houseblocks", "Value is greater than the total number of houses in the project's construction houseblocks, which is not allowed."),
+        MISSING_DATAEXCHANGE_MAPPING("missing_dataexchange_mapping", "The data exchange mapping is incomplete. A custom property is not configured for this field."),
+        NUMERIC_RANGE_VALUE("numeric_range_value", "The numeric property has a range value assigned for this project, but only single values are allowed."),
+        OWNERSHIP_RANGE_MAPPING_ERROR("ownership_range_mapping_error", "The ownership range selected cannot be mapped within one interval of the export ranges."),
+        CONFIDENTIALITY_ERROR("min_confidentiality_error", "The confidentiality level of the project is less than the minimum allowed export confidentiality.");
+
+        private final String errorCode;
+        private final String errorMessage;
+
+        EXPORT_ERROR(String errorCode, String errorMessage) {
+            this.errorCode = errorCode;
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorCode() {
+            return errorCode;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
 }
