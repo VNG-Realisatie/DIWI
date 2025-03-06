dac9dbba70976b145bd493c03138c693a1cc02f0
Emiel Rombouts <erombouts@phinion.com>
2025-03-06 15:45:07 +0100

    Add polygon and sums

--- backend/src/main/java/nl/vng/diwi/services/export/gelderland/GdbGelderlandExport.java
+++ backend/src/main/java/nl/vng/diwi/services/export/gelderland/GdbGelderlandExport.java
@@ -1,6 +1,7 @@
 package nl.vng.diwi.services.export.gelderland;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -10,21 +11,25 @@ import java.util.stream.Collectors;
 import org.geojson.Crs;
 import org.geojson.Feature;
 import org.geojson.FeatureCollection;
+import org.geojson.MultiPolygon;
 import org.geojson.jackson.CrsType;
 
 import jakarta.ws.rs.core.StreamingOutput;
 import nl.vng.diwi.dal.entities.ProjectExportSqlModel;
 import nl.vng.diwi.dal.entities.ProjectExportSqlModelExtended;
 import nl.vng.diwi.dal.entities.enums.Confidentiality;
+import nl.vng.diwi.dal.entities.enums.MutationType;
 import nl.vng.diwi.dal.entities.enums.OwnershipType;
 import nl.vng.diwi.dal.entities.enums.PlanType;
 import nl.vng.diwi.dal.entities.enums.ProjectPhase;
 import nl.vng.diwi.generic.Constants;
+import nl.vng.diwi.generic.Json;
 import nl.vng.diwi.models.ConfigModel;
 import nl.vng.diwi.models.DataExchangePropertyModel;
 import nl.vng.diwi.models.PropertyModel;
 import nl.vng.diwi.security.LoggedUser;
 import nl.vng.diwi.services.DataExchangeExportError;
+import nl.vng.diwi.services.export.ExportUtil;
 
 public class GdbGelderlandExport {
     public static StreamingOutput buildExportObject(
@@ -60,7 +65,11 @@ public class GdbGelderlandExport {
 
         // Convert GeoJSON and CSV to GDB here
 
-        throw new UnsupportedOperationException("Unimplemented method 'buildExportObject'");
+        // For now just return the json
+        return output -> {
+            Json.mapper.writeValue(output, exportObject);
+            output.flush();
+        };
     }
 
     public static Feature getProjectFeature(
@@ -79,6 +88,11 @@ public class GdbGelderlandExport {
             LoggedUser user) {
 
         Feature feature = new Feature();
+
+        MultiPolygon multiPolygon = ExportUtil.createPolygonForProject(project.getGeometries(), targetCrs, project.getProjectId());
+        if (!multiPolygon.getCoordinates().isEmpty()) {
+            feature.setGeometry(multiPolygon);
+        }
 
         feature.setProperty("OBJECTID", objectId);
         feature.setProperty("GlobalID", project.getProjectId());
@@ -113,34 +127,23 @@ public class GdbGelderlandExport {
         // knelpunten _meerkeuze // custom prop
         // toelichting_knelpunten // custom prop
         // verhuurder_type // custom prop
-        feature.setProperty("aantal_huurwoningen_corporatie", getSums(project));
         // opmerkingen_kwalitatief // custom prop
         feature.setProperty("koppelid", project.getProjectId());
 
         // klopt_geom // has note check with province
         // SHAPE_Length // Auto generated?
         // SHAPE_Area // Auto generated?
-        // Totaal_bouw
-        // Totaal_gerealiseerd
-        // Totaal_resterend
-        // Totaal_sloop
-        // Totaal_sloop_gerealiseerd
-        // Totaal_sloop_resterend
-        // Totaal_netto
-        // Totaal_netto_gerealiseerd
-        // Totaal_netto_resterend
-        // Totaal_eengezins_resterend
-        // Totaal_meergezins_resterend
-        // Totaal_type_onbekend_resterend
-        // Totaal_koop_resterend
-        // Totaal_huur_resterend
-        // Totaal_koop_huur_onbekend_resterend
-        // overkoepelende_plan_id
-        // overkoepelende_plan_naam
-        // aantal_tijdelijke_woningen
-        // aantal_nultreden_woningen
-        // aantal_geclusterde_woningen
-        // aantal_zorggeschikte_woningen
+        var sums = getSums(project, exportDate);
+        for (var sum : sums.entrySet()) {
+            feature.setProperty(sum.getKey(), sum.getValue());
+        }
+
+        feature.setProperty("overkoepelende_plan_id", null);
+        feature.setProperty("overkoepelende_plan_naam", null);
+        // aantal_tijdelijke_woningen // custom prop
+        // aantal_nultreden_woningen // custom prop
+        // aantal_geclusterde_woningen // custom prop
+        // aantal_zorggeschikte_woningen // custom prop
         // Totaal_koop1
         // Totaal_koop2
         // Totaal_koop3
@@ -152,27 +155,120 @@ public class GdbGelderlandExport {
         // Totaal_huur4
         // Totaal_huur_onbekend
         // Totaal_eigendom_onbekend
-        // energieconcept
-        // tapwatervoorziening
-        // realisatiekans
-        // aandachtsgroepen
-        // sleutelproject
+        // energieconcept // custom prop
+        // tapwatervoorziening // custom prop
+        // realisatiekans // custom prop
+        // aandachtsgroepen// custom prop
+        // sleutelproject // custom prop
         // aantal_middenhuur_corporatie
-        // onzelfstandige_wooneenheden
+        // onzelfstandige_wooneenheden // custom prop
 
         return feature;
     }
 
-    private static Integer getSums(ProjectExportSqlModelExtended project) {
-        int corporation = 0;
+    private static Map<String, Object> getSums(ProjectExportSqlModelExtended project, LocalDate exportDate) {
+        int Totaal_aantal_huurwoningen_corporatie = 0;
+        int Totaal_bouw = 0;
+        int Totaal_gerealiseerd = 0;
+        int Totaal_resterend = 0;
+        int Totaal_sloop = 0;
+        int Totaal_sloop_gerealiseerd = 0;
+        int Totaal_sloop_resterend = 0;
+        int Totaal_netto = 0;
+        int Totaal_netto_gerealiseerd = 0;
+        int Totaal_netto_resterend = 0;
+        int Totaal_eengezins_resterend = 0;
+        int Totaal_meergezins_resterend = 0;
+        int Totaal_type_onbekend_resterend = 0;
+        int Totaal_koop_resterend = 0;
+        int Totaal_huur_resterend = 0;
+        int Totaal_koop_huur_onbekend_resterend = 0;
+
         for (var b : project.getHouseblocks()) {
-            for (var o : b.getOwnershipValueList()) {
-                if (o.getOwnershipType() == OwnershipType.HUURWONING_WONINGCORPORATIE) {
-                    o.getOwnershipAmount();
+            final boolean gerealiseerd = b.getEndDate().isBefore(exportDate);
+
+            int resterend = 0;
+            boolean bouw = b.getMutationKind() == MutationType.CONSTRUCTION;
+
+            if (bouw) {
+                Totaal_bouw += b.getMutationAmount();
+                if (gerealiseerd) {
+                    Totaal_gerealiseerd += b.getMutationAmount();
+                } else {
+                    resterend += b.getMutationAmount();
+                    Totaal_meergezins_resterend += b.getMeergezinswoning();
+                    Totaal_eengezins_resterend += b.getEengezinswoning();
+                    Totaal_type_onbekend_resterend = b.getMutationAmount() - b.getMeergezinswoning() - b.getEengezinswoning();
+                }
+            } else {
+                Totaal_sloop += b.getMutationAmount();
+                if (gerealiseerd) {
+                    Totaal_sloop_gerealiseerd += b.getMutationAmount();
+                } else {
+                    Totaal_sloop_resterend += b.getMutationAmount();
                 }
             }
+
+            int huur_resterend = 0;
+            int koop_resterend = 0;
+            for (var o : b.getOwnershipValueList()) {
+                if (o.getOwnershipType() == OwnershipType.HUURWONING_WONINGCORPORATIE) {
+                    Totaal_aantal_huurwoningen_corporatie += o.getOwnershipAmount();
+                    if (!gerealiseerd) {
+                        if (bouw) {
+                            huur_resterend += o.getOwnershipAmount();
+                        } else {
+                            huur_resterend -= o.getOwnershipAmount();
+                        }
+                    }
+                } else if (o.getOwnershipType() == OwnershipType.HUURWONING_PARTICULIERE_VERHUURDER) {
+                    if (!gerealiseerd) {
+                        if (bouw) {
+                            huur_resterend += o.getOwnershipAmount();
+                        } else {
+                            huur_resterend -= o.getOwnershipAmount();
+                        }
+                    }
+
+                } else if (o.getOwnershipType() == OwnershipType.KOOPWONING) {
+                    if (!gerealiseerd) {
+                        if (bouw) {
+                            koop_resterend += o.getOwnershipAmount();
+                        } else {
+                            koop_resterend -= o.getOwnershipAmount();
+                        }
+                    }
+                }
+            }
+
+            Totaal_koop_huur_onbekend_resterend += resterend - huur_resterend - koop_resterend;
+            Totaal_koop_resterend += koop_resterend;
+            Totaal_huur_resterend += huur_resterend;
+            Totaal_resterend += resterend;
         }
-        return corporation;
+
+        Totaal_netto = Totaal_bouw - Totaal_sloop;
+        Totaal_netto_gerealiseerd = Totaal_gerealiseerd - Totaal_sloop_gerealiseerd;
+        Totaal_netto_resterend = Totaal_resterend - Totaal_sloop_resterend;
+
+        var map = new HashMap<String, Object>();
+        map.put("aantal_huurwoningen_corporatie", Totaal_aantal_huurwoningen_corporatie);
+        map.put("Totaal_bouw", Totaal_bouw);
+        map.put("Totaal_gerealiseerd", Totaal_gerealiseerd);
+        map.put("Totaal_resterend", Totaal_resterend);
+        map.put("Totaal_sloop", Totaal_sloop);
+        map.put("Totaal_sloop_gerealiseerd", Totaal_sloop_gerealiseerd);
+        map.put("Totaal_sloop_resterend", Totaal_sloop_resterend);
+        map.put("Totaal_netto", Totaal_netto);
+        map.put("Totaal_netto_gerealiseerd", Totaal_netto_gerealiseerd);
+        map.put("Totaal_netto_resterend", Totaal_netto_resterend);
+        map.put("Totaal_eengezins_resterend", Totaal_eengezins_resterend);
+        map.put("Totaal_meergezins_resterend", Totaal_meergezins_resterend);
+        map.put("Totaal_type_onbekend_resterend", Totaal_type_onbekend_resterend);
+        map.put("Totaal_koop_resterend", Totaal_koop_resterend);
+        map.put("Totaal_huur_resterend", Totaal_huur_resterend);
+        map.put("Totaal_koop_huur_onbekend_resterend", Totaal_koop_huur_onbekend_resterend);
+        return map;
 
     }
 
--- backend/src/test/java/nl/vng/diwi/services/export/gelderland/GdbGelderlandExportTest.java
+++ backend/src/test/java/nl/vng/diwi/services/export/gelderland/GdbGelderlandExportTest.java
@@ -1,7 +1,6 @@
 package nl.vng.diwi.services.export.gelderland;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static nl.vng.diwi.dal.entities.ProjectExportSqlModelExtended.*;
 
 import java.io.File;
 import java.time.LocalDate;
@@ -13,16 +12,14 @@ import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import org.apache.logging.log4j.core.util.UuidUtil;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
-import com.fasterxml.jackson.databind.ObjectWriter;
-
-import nl.vng.diwi.dal.entities.ProjectExportSqlModel;
 import nl.vng.diwi.dal.entities.ProjectExportSqlModelExtended;
+import nl.vng.diwi.dal.entities.ProjectExportSqlModelExtended.*;
 import nl.vng.diwi.dal.entities.enums.Confidentiality;
+import nl.vng.diwi.dal.entities.enums.MutationType;
 import nl.vng.diwi.dal.entities.enums.ObjectType;
 import nl.vng.diwi.dal.entities.enums.OwnershipType;
 import nl.vng.diwi.dal.entities.enums.PlanType;
@@ -34,54 +31,84 @@ import nl.vng.diwi.generic.ResourceUtil;
 import nl.vng.diwi.models.ConfigModel;
 import nl.vng.diwi.models.DataExchangePropertyModel;
 import nl.vng.diwi.models.PropertyModel;
-import nl.vng.diwi.services.DataExchangeExportError;
 import nl.vng.diwi.security.LoggedUser;
+import nl.vng.diwi.services.DataExchangeExportError;
+import nl.vng.diwi.testutil.ProjectsUtil;
 
 public class GdbGelderlandExportTest {
     @Test
     void testGetProjectFeature() throws Exception {
+        // Some constants
         ConfigModel configModel = new ConfigModel();
-        ProjectExportSqlModelExtended project = ProjectExportSqlModelExtended.builder()
-                .projectId(UUID.fromString("fdd87435-025f-48ed-a2b4-d765246040cd"))
-                .name("project name")
-                .confidentiality(Confidentiality.EXTERNAL_GOVERNMENTAL)
-                .projectPhase(ProjectPhase._5_PREPARATION)
-                .planType(List.of(PlanType.TRANSFORMATIEGEBIED))
-                .houseblocks(List.of(
-                        HouseblockExportSqlModel.builder()
-                                .name("block1")
-                                .ownershipValueList(List.of(
-                                        OwnershipValueSqlModel.builder()
-                                                .ownershipAmount(1)
-                                                .ownershipType(OwnershipType.HUURWONING_WONINGCORPORATIE)
-                                                .build()))
-                                .endDate(LocalDate.of(2025, 3, 1))
-                                .build(),
-                        HouseblockExportSqlModel.builder()
-                                .name("block2")
-                                .endDate(LocalDate.of(2025, 3, 2))
-                                .build(),
-                        HouseblockExportSqlModel.builder()
-                                .name("block3")
-                                .endDate(LocalDate.of(2025, 3, 3))
-                                .build()))
+        LocalDate exportDate = LocalDate.of(2025, 3, 2); // Same as middle house block so we have future and past blocks
+        Confidentiality minConfidentiality = Confidentiality.EXTERNAL_REGIONAL;
+        String targetCrs = "EPSG:28992";
+        LoggedUser user = LoggedUser.builder()
+                .firstName("first")
+                .lastName("last")
                 .build();
 
+        // Create some custom props
         List<PropertyModel> customProps = List.of(PropertyModel.builder()
                 .name("text")
                 .objectType(ObjectType.PROJECT)
                 .propertyType(PropertyType.TEXT)
                 .build());
 
-        Map<String, DataExchangePropertyModel> dxPropertiesMap = new HashMap<>();
-        LocalDate exportDate = LocalDate.of(2025, 3, 6);
-        Confidentiality minConfidentiality = Confidentiality.EXTERNAL_REGIONAL;
-        List<DataExchangeExportError> errors = new ArrayList<>();
-        String targetCrs = "EPSG:28992";
-        LoggedUser user = LoggedUser.builder()
-                .firstName("first")
-                .lastName("last")
+        // Create a project with some blocks
+        ProjectExportSqlModelExtended project = ProjectExportSqlModelExtended.builder()
+                .projectId(UUID.fromString("fdd87435-025f-48ed-a2b4-d765246040cd"))
+                .name("project name")
+                .geometries(List.of(ProjectsUtil.PLOT_JSON_STRING))
+                .confidentiality(Confidentiality.EXTERNAL_GOVERNMENTAL)
+                .projectPhase(ProjectPhase._5_PREPARATION)
+                .planType(List.of(PlanType.TRANSFORMATIEGEBIED))
+
+                .houseblocks(List.of(
+                        HouseblockExportSqlModel.builder()
+                                .name("block1")
+                                .mutationAmount(4)
+                                .mutationKind(MutationType.CONSTRUCTION)
+                                .ownershipValueList(List.of(
+                                        OwnershipValueSqlModel.builder()
+                                                .ownershipAmount(4)
+                                                .ownershipType(OwnershipType.HUURWONING_WONINGCORPORATIE)
+                                                .build()))
+                                .meergezinswoning(4)
+                                .eengezinswoning(0)
+                                .endDate(LocalDate.of(2025, 3, 1))
+                                .build(),
+                        HouseblockExportSqlModel.builder()
+                                .name("block2")
+                                .mutationAmount(2)
+                                .mutationKind(MutationType.CONSTRUCTION)
+                                .ownershipValueList(List.of(
+                                        OwnershipValueSqlModel.builder()
+                                                .ownershipAmount(2)
+                                                .ownershipType(OwnershipType.HUURWONING_PARTICULIERE_VERHUURDER)
+                                                .build()))
+                                .meergezinswoning(0)
+                                .eengezinswoning(2)
+                                .endDate(LocalDate.of(2025, 3, 2))
+                                .build(),
+                        HouseblockExportSqlModel.builder()
+                                .name("block3")
+                                .mutationAmount(1)
+                                .mutationKind(MutationType.DEMOLITION)
+                                .ownershipValueList(List.of(
+                                        OwnershipValueSqlModel.builder()
+                                                .ownershipAmount(1)
+                                                .ownershipType(OwnershipType.KOOPWONING)
+                                                .build()))
+                                .meergezinswoning(0)
+                                .eengezinswoning(1)
+                                .endDate(LocalDate.of(2025, 3, 3))
+                                .build()))
                 .build();
+
+        Map<String, DataExchangePropertyModel> dxPropertiesMap = new HashMap<>();
+
+        List<DataExchangeExportError> errors = new ArrayList<>();
 
         PropertyModel priceRangeBuyFixedProp = customProps.stream()
                 .filter(pfp -> pfp.getName().equals(Constants.FIXED_PROPERTY_PRICE_RANGE_BUY)).findFirst().orElse(null);
@@ -106,8 +133,9 @@ public class GdbGelderlandExportTest {
                 17,
                 user);
 
-        Json.writerWithDefaultPrettyPrinter.writeValue(new File("src/test/resources/GdbGelderlandTest/feature.actual.json"), result);
+        assertThat(errors).isEmpty();
 
+        Json.writerWithDefaultPrettyPrinter.writeValue(new File("src/test/resources/GdbGelderlandTest/feature.actual.json"), result);
         var expected = ResourceUtil.getResourceAsString("GdbGelderlandTest/feature.expected.json");
 
         JSONAssert.assertEquals(expected, Json.mapper.writeValueAsString(result), JSONCompareMode.NON_EXTENSIBLE);
