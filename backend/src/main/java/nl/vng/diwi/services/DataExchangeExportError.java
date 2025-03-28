b93c33341182d8f960c41d7df63b35ad2ffc03fb
Emiel Rombouts <erombouts@phinion.com>
2025-03-28 15:51:38 +0100

    Simplify DataExchangeExportError a bit by not storing derived data

--- backend/src/main/java/nl/vng/diwi/services/DataExchangeExportError.java
+++ backend/src/main/java/nl/vng/diwi/services/DataExchangeExportError.java
@@ -1,39 +1,49 @@
 package nl.vng.diwi.services;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import nl.vng.diwi.dal.entities.enums.OwnershipCategory;
 import lombok.Getter;
 import lombok.AccessLevel;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
+@Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class DataExchangeExportError {
 
     private UUID projectId;
     private UUID houseblockId;
     private String fieldName;
-    private String code;
-    private String message;
+    private EXPORT_ERROR error;
     private OwnershipCategory cat1;
     private OwnershipCategory cat2;
     private Long priceValueMin;
     private Long priceValueMax;
 
     public DataExchangeExportError(UUID projectId, String fieldName, EXPORT_ERROR exportError) {
-        this(projectId, null, fieldName, exportError.getErrorCode(), exportError.getErrorMessage(), null, null, null, null);
+        this(projectId, null, fieldName, exportError, null, null, null, null);
     }
 
     public DataExchangeExportError(UUID projectId, UUID houseblockId, EXPORT_ERROR exportError) {
-        this(projectId, houseblockId, null, exportError.getErrorCode(), exportError.getErrorMessage(), null, null, null, null);
+        this(projectId, houseblockId, null, exportError, null, null, null, null);
     }
 
     public DataExchangeExportError(UUID projectId, UUID houseblockId, EXPORT_ERROR exportError, OwnershipCategory cat1, OwnershipCategory cat2, Long priceValueMin, Long priceValueMax) {
-        this(projectId, houseblockId, null, exportError.getErrorCode(), exportError.getErrorMessage(), cat1, cat2, priceValueMin, priceValueMax);
+        this(projectId, houseblockId, null, exportError, cat1, cat2, priceValueMin, priceValueMax);
+    }
+
+    public String getCode(){
+        return error.getErrorCode();
+    }
+
+    public String getMessage(){
+        return error.getErrorMessage();
     }
 
     @Getter
@@ -44,7 +54,8 @@ public class DataExchangeExportError {
         MISSING_DATAEXCHANGE_MAPPING("missing_dataexchange_mapping", "The data exchange mapping is incomplete. A custom property is not configured for this field."),
         NUMERIC_RANGE_VALUE("numeric_range_value", "The numeric property has a range value assigned for this project, but only single values are allowed."),
         OWNERSHIP_RANGE_MAPPING_ERROR("ownership_range_mapping_error", "The ownership range selected cannot be mapped within one interval of the export ranges."),
-        CONFIDENTIALITY_ERROR("min_confidentiality_error", "The confidentiality level of the project is less than the minimum allowed export confidentiality.");
+        CONFIDENTIALITY_ERROR("min_confidentiality_error", "The confidentiality level of the project is less than the minimum allowed export confidentiality."),
+        PROJECT_DOES_NOT_HAVE_GEOMETRY("project_does_not_have_geometry", "Project doesn't have geometry");
 
         private final String errorCode;
         private final String errorMessage;
